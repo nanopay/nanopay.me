e82a52d8eac2a371e9f3c8043f8e30248e986fb0
anarkrypto <anarkrypto@gmail.com>
2023-03-05 17:49:39 -0300

    💄 Set default background to slate-50

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -2,7 +2,7 @@ import { Head, Html, Main, NextScript } from 'next/document'
 
 export default function Document() {
 	return (
-		<Html className="h-full bg-gray-50 antialiased" lang="en">
+		<Html className="h-full bg-slate-50 antialiased" lang="en">
 			<Head />
 			<body className="flex h-full flex-col">
 				<Main />
