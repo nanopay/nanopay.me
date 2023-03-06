0efc21f9353b58679a78c643cabd1e9f1da2d943
anarkrypto <anarkrypto@gmail.com>
2023-03-05 22:32:38 -0300

    🔧 Allow githubusercontent.com as avatar source

--- next.config.js
+++ next.config.js
@@ -4,6 +4,15 @@ const nextConfig = {
 	experimental: {
 		scrollRestoration: true,
 	},
+	images: {
+		remotePatterns: [
+			{
+				protocol: 'https',
+				hostname: 'avatars.githubusercontent.com',
+				pathname: '/u/**',
+			},
+		],
+	},
 }
 
 module.exports = nextConfig
