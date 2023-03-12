09cea4aea66074afe27a55e91d54b6e8aa26f161
anarkrypto <anarkrypto@gmail.com>
2023-03-12 01:15:51 -0300

    🚧 Validate users before generate presigned upload

--- src/pages/api/users/upload/avatar.ts
+++ src/pages/api/users/upload/avatar.ts
@@ -1,9 +1,29 @@
 import { createPresigned } from '@/services/s3'
+import { Database } from '@/types/supabase'
+import { createServerSupabaseClient } from '@supabase/auth-helpers-nextjs'
 import { NextApiRequest, NextApiResponse } from 'next'
 
 export default async function (req: NextApiRequest, res: NextApiResponse) {
+	const supabaseServerClient = createServerSupabaseClient<Database>({
+		req,
+		res,
+	})
+
+	const {
+		data: { user },
+		error: userError,
+	} = await supabaseServerClient.auth.getUser()
+
+	if (userError) {
+		return res.status(500).json({ message: userError.message })
+	}
+
+	if (!user) {
+		return res.status(401).json({ message: 'Unauthorized' })
+	}
+
 	const { url, fields } = await createPresigned({
-		key: `users/${req.body.id}/tmp/avatar.png`,
+		key: `users/${user.id}/tmp/avatar.png`,
 		minLength: 1024, // 1KB
 		maxLength: 1024 * 1024 * 5, // 5MB
 	})
