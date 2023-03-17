0cfdc8fdfefa11adc92842a7394012a65045cce6
anarkrypto <anarkrypto@gmail.com>
2023-03-17 12:54:32 -0300

    ✨ Add verifyApiKey util

--- src/utils/apiKey.ts
+++ src/utils/apiKey.ts
@@ -32,3 +32,40 @@ export const createApiKey = () => {
 
 	return { apiKey, checksum }
 }
+
+export const verifyApiKey = (apiKey: string) => {
+	if (
+		!API_KEYS_SECRET ||
+		!isHexString(API_KEYS_SECRET) ||
+		API_KEYS_SECRET.length != API_KEYS_SECRET_LENGTH
+	) {
+		throw new Error('API_KEYS_SECRET is not set or is not a valid hex string')
+	}
+
+	if (!isHexString(apiKey)) {
+		return false
+	}
+
+	if (
+		apiKey.length !=
+		API_KEY_BYTES_LENGTH * 2 + API_KEY_CHECKSUM_BYTES_LENGTH * 2
+	) {
+		return false
+	}
+
+	const randomBytes = getBytes(
+		apiKey.slice(0, -API_KEY_CHECKSUM_BYTES_LENGTH * 2),
+	)
+
+	const givenChecksum = apiKey.slice(-API_KEY_CHECKSUM_BYTES_LENGTH * 2)
+
+	const secretKeyBytes = getBytes(API_KEYS_SECRET)
+
+	const computedChecksum = blake2bHex(
+		randomBytes,
+		secretKeyBytes,
+		API_KEY_CHECKSUM_BYTES_LENGTH,
+	)
+
+	return givenChecksum === computedChecksum
+}
