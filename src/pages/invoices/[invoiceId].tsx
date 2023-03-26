b2f90923ae06103d2675e3a09384101556c75ed2
anarkrypto <anarkrypto@gmail.com>
2023-03-25 21:32:35 -0300

    ✨ Implement payment detect with Pusher

--- src/pages/invoices/[invoiceId].tsx
+++ src/pages/invoices/[invoiceId].tsx
@@ -1,16 +1,45 @@
 import Checkout from '@/components/Checkout'
 import { Invoice } from '@/types/invoice'
+import { useEffect, useState } from 'react'
+import Pusher from 'pusher-js'
 import { GetServerSidePropsContext } from 'next'
 
+interface PaymentEvent {
+	from: string
+	to: string
+	amount: number
+	timestamp: number
+}
+
 export default function PayInvoice({ invoice }: { invoice: Invoice }) {
+	if (!invoice) return <div>Invoice not found</div>
+
+	const [paid, setPaid] = useState(invoice.status === 'paid')
+
+	useEffect(() => {
+		Pusher.logToConsole = true
+		const pusher = new Pusher(process.env.NEXT_PUBLIC_PUSHER_KEY as string, {
+			cluster: 'us2',
+		})
+		const channel = pusher.subscribe(invoice.id.toString())
+		channel.bind('payment', function (data: PaymentEvent) {
+			setPaid(true)
+		})
+
+		return () => {
+			channel.unbind_all()
+			channel.unsubscribe()
+		}
+	}, [])
+
 	return (
 		<div className="w-full max-w-3xl mx-auto h-screen flex sm:items-center justify-center">
 			<Checkout
-				invoiceId={invoice.id}
+				invoiceId={invoice.id.toString()}
 				address={invoice.pay_address}
 				amount={invoice.price}
 				usd={1}
-				paid={false}
+				paid={paid}
 				expiresAt={new Date(invoice.expires_at)}
 			/>
 		</div>
