099945a572be10c7e98da5b8950663e04d5842bb
anarkrypto <anarkrypto@gmail.com>
2023-04-11 10:41:36 -0300

    🔧 Use only NEXT_PUBLIC_SUPABASE_URL

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,7 +1,7 @@
 import { Database } from '@/types/supabase'
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = process.env.SUPABASE_URL || ''
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || ''
 const supabaseKey = process.env.SUPABASE_KEY || ''
 
 export const supabase = createClient<Database>(supabaseUrl, supabaseKey)
