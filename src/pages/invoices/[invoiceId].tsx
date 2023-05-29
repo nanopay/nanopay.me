8a101a43ac91a8467b726d9f1c071328276ed110
anarkrypto <anarkrypto@gmail.com>
2023-05-29 00:02:59 -0300

    🐛 Auto unsubscribe to pusher after expires - Do not subscribe when invoice already exipred

--- src/pages/invoices/[invoiceId].tsx
+++ src/pages/invoices/[invoiceId].tsx
@@ -23,8 +23,13 @@ export default function PayInvoice({
 }: PayInvoiceProps) {
 	const [payments, setPayments] = useState<Payment[]>(_payments)
 
+	const amountPaid = payments.reduce((acc, curr) => acc + curr.amount, 0)
+	const paid = amountPaid >= invoice.price
+	const timeLeft = new Date(invoice.expires_at).getTime() - new Date().getTime()
+	const isExpired = !paid && timeLeft <= 0
+
 	useEffect(() => {
-		if (!invoice) return
+		if (!invoice || paid || isExpired) return
 		Pusher.logToConsole = true
 		const pusher = new Pusher(process.env.NEXT_PUBLIC_PUSHER_KEY as string, {
 			cluster: 'us2',
@@ -36,6 +41,12 @@ export default function PayInvoice({
 		channel.bind('invoice.partially_paid', (data: PaymentNotification) => {
 			setPayments([...payments, ...data.payments])
 		})
+
+		// auto unsubscribe when expires
+		setTimeout(() => {
+			channel.unbind_all()
+			channel.unsubscribe()
+		}, timeLeft)
 
 		return () => {
 			channel.unbind_all()
