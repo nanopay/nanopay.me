479f8ffa6c3426dd057924e7a5220eac3868bddd
anarkrypto <anarkrypto@gmail.com>
2023-06-08 17:15:03 -0300

    🐛 Fix redirect loading

--- src/app/redirect/loading.tsx
+++ src/app/redirect/loading.tsx
@@ -1,6 +1,8 @@
 'use client'
 
-export default function Loading() {
+import Loading from '@/components/Loading'
+
+export default function RedirectLoading() {
 	return (
 		<div className="w-full h-screen flex justify-center items-center">
 			<Loading />
