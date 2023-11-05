5344e6f2945f9dbcbe9efa4dbd54c4e05d263a43
anarkrypto <anarkrypto@gmail.com>
2023-11-05 20:57:55 -0300

    🚧 Implement Sign In with Password

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import { Container } from '@/components/Container'
-import { useSearchParams } from 'next/navigation'
+import { useSearchParams, useRouter } from 'next/navigation'
 import { getURL } from '@/utils/helpers'
 import Link from 'next/link'
 import { Logo } from '@/components/Logo'
@@ -11,6 +11,25 @@ import Image from 'next/image'
 import GithubSVG from '@/images/logos/github.svg'
 import { Button } from '@/components/Button'
 import Input from '@/components/Input'
+import { ajvResolver } from '@hookform/resolvers/ajv'
+import { JSONSchemaType } from 'ajv'
+import { fullFormats } from 'ajv-formats/dist/formats'
+import { Controller, useForm } from 'react-hook-form'
+import { useToast } from '@/hooks/useToast'
+
+interface AuthEmailPassword {
+	email: string
+	password: string
+}
+
+const schema: JSONSchemaType<AuthEmailPassword> = {
+	type: 'object',
+	properties: {
+		email: { type: 'string', format: 'email', maxLength: 128 },
+		password: { type: 'string', minLength: 8, maxLength: 64 },
+	},
+	required: ['email', 'password'],
+}
 
 export default function LoginPage() {
 	const supabase = createBrowserSupabaseClient<Database>()
@@ -21,13 +40,44 @@ export default function LoginPage() {
 		typeof redirectedFrom === 'string' ? redirectedFrom : '/home',
 	)}`
 
-	const sign = async () => {
+	const { showError, showSuccess } = useToast()
+
+	const router = useRouter()
+
+	const {
+		control,
+		handleSubmit,
+		formState: { errors, isSubmitting },
+	} = useForm<AuthEmailPassword>({
+		defaultValues: {
+			email: '',
+			password: '',
+		},
+		resolver: ajvResolver(schema, {
+			formats: fullFormats,
+		}),
+	})
+
+	const signWithGithub = async () => {
 		await supabase.auth.signInWithOAuth({
 			provider: 'github',
 			options: {
 				redirectTo,
 			},
 		})
+	}
+
+	const signWithPassword = async ({ email, password }: AuthEmailPassword) => {
+		const { error, data } = await supabase.auth.signInWithPassword({
+			email,
+			password,
+		})
+		if (error) {
+			showError(error.message)
+			return
+		}
+		showSuccess('Success')
+		await router.push('/home')
 	}
 
 	return (
@@ -38,22 +88,47 @@ export default function LoginPage() {
 				</Link>
 			</div>
 			<div className="w-full flex flex-col space-y-6 px-2 sm:px-4 divide-y divide-slate-200">
-				<Button color="slate" onClick={sign} variant="outline">
+				<Button color="slate" onClick={signWithGithub} variant="outline">
 					<div className="flex items-center space-x-2">
 						<Image src={GithubSVG} width={20} height={20} alt="github icon" />
 						<span>Sign in with Github</span>
 					</div>
 				</Button>
 				<div className="py-6 w-full">
-					<Input
-						label="Email"
-						className="w-full rounded !bg-transparent !text-white"
+					<Controller
+						name="email"
+						control={control}
+						render={({ field }) => (
+							<Input
+								label="E-mail"
+								{...field}
+								errorMessage={errors.email?.message}
+								className="w-full !bg-transparent"
+							/>
+						)}
 					/>
-					<Input
-						label="Password"
-						className="w-full rounded !bg-transparent !text-white"
+
+					<Controller
+						name="password"
+						control={control}
+						render={({ field }) => (
+							<Input
+								label="Password"
+								type="password"
+								{...field}
+								onChange={e => field.onChange(e.target.value.slice(0, 40))}
+								errorMessage={errors.password?.message}
+								className="w-full !bg-transparent"
+							/>
+						)}
 					/>
-					<Button color="nano" onClick={sign} className="w-full">
+
+					<Button
+						color="nano"
+						onClick={handleSubmit(signWithPassword)}
+						className="w-full"
+						disabled={isSubmitting}
+					>
 						Sign In
 					</Button>
 					<div className="pt-6 flex flex-col items-center">
