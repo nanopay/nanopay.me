b4f3738cda8e0d33999d90c711e8654ed5f17c24
anarkrypto <anarkrypto@gmail.com>
2023-11-09 01:33:11 -0300

    ♻️ Move index page to App dir

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,16 @@
-import Head from 'next/head'
+'use client'
+
 import { Faqs } from '@/components/Faqs'
 import { Footer } from '@/components/Footer'
 import { Header } from '@/components/Header'
 import { Hero } from '@/components/Hero'
 import { useAuth } from '@/contexts/AuthProvider'
 
-export default function Home() {
+export default function Index() {
 	const { user } = useAuth()
 
 	return (
 		<>
-			<Head>
-				<title>NanoPay.me - Pay and receive payments with Nano.</title>
-				<meta
-					name="description"
-					content="A payment gateway for Nano (XNO). Open source, simple to use and no fees."
-				/>
-			</Head>
 			<Header size="lg" user={user} />
 			<main>
 				<Hero />
