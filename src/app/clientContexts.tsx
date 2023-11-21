1728a5e62909574bed852458ad5ff0ac5fc5bae7
anarkrypto <anarkrypto@gmail.com>
2023-11-21 19:46:19 -0300

    ♻️ Remove AuthProvider from clientContexts

--- src/app/clientContexts.tsx
+++ src/app/clientContexts.tsx
@@ -6,21 +6,18 @@ import { ThemeProvider } from '@mui/material'
 import { ToastContainer } from 'react-toastify'
 import { QueryClient, QueryClientProvider } from 'react-query'
 import PreferencesProvider from '@/contexts/PreferencesProvider'
-import { AuthProvider } from '@/contexts/AuthProvider'
 
 const queryClient = new QueryClient()
 
 export default function ClientContexts({ children }: { children: ReactNode }) {
 	return (
 		<QueryClientProvider client={queryClient}>
-			<AuthProvider>
-				<ThemeProvider theme={theme}>
-					<PreferencesProvider>
-						{children}
-						<ToastContainer />
-					</PreferencesProvider>
-				</ThemeProvider>
-			</AuthProvider>
+			<ThemeProvider theme={theme}>
+				<PreferencesProvider>
+					{children}
+					<ToastContainer />
+				</PreferencesProvider>
+			</ThemeProvider>
 		</QueryClientProvider>
 	)
 }
