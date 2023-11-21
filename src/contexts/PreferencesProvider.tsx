6d0685a92d7dad96245218678add70c0a7fc6690
anarkrypto <anarkrypto@gmail.com>
2023-11-21 19:51:14 -0300

    ✨ Add sidebar state to PreferencesProvider

--- src/contexts/PreferencesProvider.tsx
+++ src/contexts/PreferencesProvider.tsx
@@ -3,6 +3,8 @@ import { createContext, useContext, useState } from 'react'
 interface PreferencesContextValues {
 	showPreReleaseAlert: boolean
 	setShowPreReleaseAlert: (value: boolean) => void
+	sidebarOpen: boolean
+	setSidebarOpen: (value: boolean) => void
 }
 
 interface PreferencesProviderProps {
@@ -13,12 +15,15 @@ const PreferencesContext = createContext({} as PreferencesContextValues)
 
 export const PreferencesProvider = ({ children }: PreferencesProviderProps) => {
 	const [showPreReleaseAlert, setShowPreReleaseAlert] = useState(true)
+	const [sidebarOpen, setSidebarOpen] = useState(false)
 
 	return (
 		<PreferencesContext.Provider
 			value={{
 				showPreReleaseAlert,
 				setShowPreReleaseAlert,
+				sidebarOpen,
+				setSidebarOpen,
 			}}
 		>
 			{children}
