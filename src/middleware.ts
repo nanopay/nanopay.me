2cf87b30d94f2a19d11250290b87b62fc6756e1a
anarkrypto <anarkrypto@gmail.com>
2023-11-24 14:26:14 -0300

    🐛 Fix cookies set in middleware missing on server component render pass

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,11 +3,19 @@ import type { NextRequest } from 'next/server'
 import { createClient } from './utils/supabase/middleware'
 
 export async function middleware(request: NextRequest) {
-	const { supabase, response } = createClient(request)
+	const { supabase, response, modified } = createClient(request)
 
 	const {
 		data: { session },
 	} = await supabase.auth.getSession()
+
+	if (modified) {
+		/*
+			Workaround to fix missing cookies from middleware in server components
+			Read more: https://github.com/vercel/next.js/issues/49442
+		*/
+		return response
+	}
 
 	const isAuthenticated = !!session
 
--- src/utils/supabase/middleware.ts
+++ src/utils/supabase/middleware.ts
@@ -14,6 +14,8 @@ export const createClient = (request: NextRequest) => {
 		},
 	})
 
+	let modified = false
+
 	const supabase = createServerClient(supabaseUrl, supabaseKey, {
 		cookies: {
 			get(name: string) {
@@ -21,21 +23,19 @@ export const createClient = (request: NextRequest) => {
 			},
 			set(name: string, value: string, options: CookieOptions) {
 				// If the cookie is updated, update the cookies for the request and response
+				console.warn('rewriting cookies for supabase ***')
 				request.cookies.set({
 					name,
 					value,
 					...options,
 				})
-				response = NextResponse.next({
-					request: {
-						headers: request.headers,
-					},
-				})
+				response = NextResponse.redirect(request.nextUrl.clone())
 				response.cookies.set({
 					name,
 					value,
 					...options,
 				})
+				modified = true
 			},
 			remove(name: string, options: CookieOptions) {
 				// If the cookie is removed, update the cookies for the request and response
@@ -44,19 +44,16 @@ export const createClient = (request: NextRequest) => {
 					value: '',
 					...options,
 				})
-				response = NextResponse.next({
-					request: {
-						headers: request.headers,
-					},
-				})
+				response = NextResponse.redirect(request.nextUrl.clone())
 				response.cookies.set({
 					name,
 					value: '',
 					...options,
 				})
+				modified = true
 			},
 		},
 	})
 
-	return { supabase, response }
+	return { supabase, response, modified }
 }
