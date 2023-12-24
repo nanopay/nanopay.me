d4993a21f35a326adbb1f6125ebce5af893d9a79
anarkrypto <anarkrypto@gmail.com>
2023-12-24 03:12:29 -0300

    ⚰️ Remove upload avatar method from user api client

--- src/services/api/users.ts
+++ src/services/api/users.ts
@@ -1,29 +1,10 @@
-import { User, UserProfile } from '@/types/users'
-import { concatURL } from '@/utils/helpers'
-import s3 from '@/services/s3'
+import { User } from '@/types/users'
 import Fetcher, { FetcherOptions } from '@/lib/fetcher'
 
 export const users = (fetcher: Fetcher) => {
 	return {
 		retrieve: (options?: FetcherOptions): Promise<User> => {
 			return fetcher.get('/user', null, options)
-		},
-		upload: {
-			avatar: async (
-				file: File,
-				progressCallback?: (progress: number) => void,
-			): Promise<string> => {
-				if (file.size > 1024 * 1024 * 5) {
-					throw new Error('File size must be less than 5MB')
-				}
-				const { url, fields } = await fetcher.post('/upload/image')
-
-				await s3.uploadObject(file, fields, url, progressCallback)
-				return concatURL(
-					`https://${process.env.NEXT_PUBLIC_STATIC_ASSETS_HOST}`,
-					fields.key,
-				)
-			},
 		},
 	}
 }
