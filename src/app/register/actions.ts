9e7aac925a6df327814d871d2600108e55b210af
anarkrypto <anarkrypto@gmail.com>
2024-01-12 16:40:42 -0300

    🎨 Improve register action

--- src/app/register/actions.ts
+++ src/app/register/actions.ts
@@ -2,31 +2,21 @@
 
 import { cookies } from 'next/headers'
 import { redirect } from 'next/navigation'
-import { createClient } from '@/utils/supabase/server'
+import { createClient, getUserId } from '@/utils/supabase/server'
 import { UserEditables } from '@/types/users'
 import { DEFAULT_AVATAR_URL } from '@/constants'
+import { Database } from '@/types/supabase'
 
 export const registerUser = async ({ name, avatar_url }: UserEditables) => {
+	const userId = await getUserId(cookies())
+
 	const supabase = createClient(cookies())
 
-	const {
-		data: { user },
-		error: authError,
-	} = await supabase.auth.getUser()
-
-	if (authError) {
-		throw new Error(authError.message)
-	}
-
-	if (!user) {
-		throw new Error('Unauthorized')
-	}
-
 	const { error: createError } = await supabase.from('profiles').insert({
-		user_id: user.id,
+		user_id: userId,
 		name,
 		avatar_url: avatar_url || DEFAULT_AVATAR_URL,
-	} as any)
+	} as Database['public']['Tables']['profiles']['Insert'])
 
 	if (createError) {
 		throw new Error(createError.message)
