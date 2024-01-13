3b5dbe1b5435dbe6561390a137ef8005beaef755
anarkrypto <anarkrypto@gmail.com>
2024-01-13 15:52:55 -0300

    add checkUUID

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -75,3 +75,11 @@ export const sanitizeSlug = (name: string) => {
 		.replace(/[^a-zA-Z0-9-_\.]/g, '')
 		.toLowerCase()
 }
+
+export const checkUUID = (str: string) => {
+	const regex = new RegExp(
+		'^[0-9a-f]{8}-[0-9a-f]{4}-4[0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$',
+		'gi',
+	)
+	return regex.test(str)
+}
