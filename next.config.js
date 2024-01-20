58359686da990221f20a2909301e3d4b07b4784c
anarkrypto <anarkrypto@gmail.com>
2024-01-20 04:51:06 -0300

    ⚰️ Remove old rewrites

--- next.config.js
+++ next.config.js
@@ -15,46 +15,6 @@ const nextConfig = {
 			},
 		],
 	},
-	async rewrites() {
-		return [
-			{
-				source: '/login',
-				destination: '/auth/login',
-			},
-			{
-				source: '/logout',
-				destination: '/auth/logout',
-			},
-			{
-				source: '/signup',
-				destination: '/auth/signup',
-			},
-			{
-				source: '/verify-email',
-				destination: '/auth/verify-email',
-			},
-			{
-				source: '/forgot-password',
-				destination: '/auth/forgot-password',
-			},
-			{
-				source: '/magic-link',
-				destination: '/auth/magic-link',
-			},
-			{
-				source: '/home',
-				destination: '/dashboard/home',
-			},
-			{
-				source: '/profile',
-				destination: '/dashboard/profile',
-			},
-			{
-				source: '/services/:path*',
-				destination: '/dashboard/services/:path*',
-			},
-		]
-	},
 }
 
 module.exports = nextConfig
