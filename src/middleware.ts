228724dfa6d4dd686914a1cf0923e513b2dc108e
anarkrypto <anarkrypto@gmail.com>
2024-01-21 07:26:12 -0300

    ♻️ Refactor middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,9 +1,29 @@
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
-import { createClient } from './utils/supabase/middleware'
-import { applySetCookie } from './utils/cookies'
+import { createClient } from '@/utils/supabase/middleware'
+import { applySetCookie } from '@/utils/cookies'
+
+const authRoutes = [
+	'/login',
+	'/signup',
+	'/magic-link',
+	'/verify-email',
+	'/forgot-password',
+	'/auth/callback',
+]
+
+const publicRoutes = ['/', '/invoices/:path*', ...authRoutes]
 
 export async function middleware(request: NextRequest) {
+	const nextUrl = request.nextUrl.clone()
+
+	if (
+		publicRoutes.includes(nextUrl.pathname) &&
+		!authRoutes.includes(nextUrl.pathname)
+	) {
+		return NextResponse.next()
+	}
+
 	const { supabase, response } = createClient(request)
 
 	const {
@@ -14,36 +34,39 @@ export async function middleware(request: NextRequest) {
 
 	const isAuthenticated = !!session
 
-	let pathname = request.nextUrl.pathname
-
 	if (isAuthenticated) {
-		if (pathname === '/login') {
-			pathname = '/home'
+		if (authRoutes.includes(nextUrl.pathname)) {
+			nextUrl.pathname = '/home'
 		} else {
 			return response
 		}
 	} else {
-		if (pathname === '/login') {
+		if (!publicRoutes.includes(nextUrl.pathname)) {
+			nextUrl.searchParams.set(`next`, nextUrl.pathname)
+			nextUrl.pathname = '/login'
+		} else {
 			return response
 		}
-		pathname = '/login'
 	}
 
-	const redirectUrl = request.nextUrl.clone()
-	redirectUrl.pathname = pathname
-	if (request.nextUrl.pathname !== '/logout') {
-		redirectUrl.searchParams.set(`redirectedFrom`, request.nextUrl.pathname)
-	}
-	return NextResponse.redirect(redirectUrl)
+	return NextResponse.redirect(nextUrl)
 }
 
 export const config = {
 	matcher: [
-		'/services/:path*',
-		'/home',
-		'/logout',
-		'/login',
-		'/register',
-		'/profile',
+		/*
+		 * Match all request paths except for the ones starting with:
+		 * - api (API routes)
+		 * - _next/static (static files)
+		 * - _next/image (image optimization files)
+		 * - favicon.ico (favicon file)
+		 */
+		{
+			source: '/((?!api|_next/static|_next/image|favicon.ico).*)',
+			missing: [
+				{ type: 'header', key: 'next-router-prefetch' },
+				{ type: 'header', key: 'purpose', value: 'prefetch' },
+			],
+		},
 	],
 }
