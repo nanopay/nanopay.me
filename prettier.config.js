a1a81fe95050d382c329ee6c14084b3c6dcf03da
anarkrypto <anarkrypto@gmail.com>
2024-02-08 02:43:27 -0300

    🔧 Move prettierrc.json config to prettier.config.js

--- prettier.config.js
+++ prettier.config.js
@@ -1,5 +1,16 @@
 module.exports = {
-	singleQuote: true,
-	semi: false,
-	plugins: [require('prettier-plugin-tailwindcss')],
+	"arrowParens": "avoid",
+	"bracketSpacing": true,
+	"endOfLine": "lf",
+	"bracketSameLine": false,
+	"jsxSingleQuote": false,
+	"printWidth": 80,
+	"proseWrap": "preserve",
+	"quoteProps": "as-needed",
+	"semi": false,
+	"singleQuote": true,
+	"tabWidth": 2,
+	"trailingComma": "all",
+	"useTabs": true,
+	plugins: [require('prettier-plugin-tailwindcss')]
 }
