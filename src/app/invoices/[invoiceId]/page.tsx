58503eeba9b5b08dac81e07a4cb48db8c0bc0246
anarkrypto <anarkrypto@gmail.com>
2024-02-18 01:25:05 -0300

    🚸 Add custom not found to invoice page

--- src/app/invoices/[invoiceId]/page.tsx
+++ src/app/invoices/[invoiceId]/page.tsx
@@ -1,13 +1,20 @@
 import api from '@/services/api'
 import PayInvoice from './invoice'
+import NotFoundImage from '@/images/not-found.svg'
+import { FetcherError } from '@/lib/fetcher'
+import Image from 'next/image'
+import Link from 'next/link'
+import { SUPPORT_EMAIL } from '@/constants'
 
-export default async function InvoicePage({
-	params: { invoiceId },
-}: {
+interface InvoicePageProps {
 	params: {
 		invoiceId: string
 	}
-}) {
+}
+
+export default async function InvoicePage({
+	params: { invoiceId },
+}: InvoicePageProps) {
 	try {
 		const invoice = await api.invoices.get(invoiceId, {
 			next: {
@@ -24,14 +31,31 @@ export default async function InvoicePage({
 
 		return <PayInvoice invoice={invoice} payments={payments} />
 	} catch (error) {
-		const message = api.getErrorMessage(error) || 'Unknown error'
-		return (
-			<div className="flex h-full items-center justify-center">
-				<div className="text-center">
-					<h1 className="mb-4 text-2xl font-bold">Error</h1>
-					<p className="text-slate-500">{message}</p>
+		console.log(error, '8**')
+
+		if (error instanceof FetcherError && error.status === 404) {
+			return (
+				<div className="flex h-screen items-center justify-center">
+					<div className="text-center">
+						<Image
+							src={NotFoundImage}
+							alt="Not found"
+							className="mx-auto mb-4 h-80 w-80"
+						/>
+						<h1 className="mb-4 text-2xl font-bold">Invoice not found</h1>
+						<p className="text-slate-500">
+							The invoice you are looking for does not exist.
+						</p>
+						<div className="hover:text-nano mt-16 text-sm text-slate-500">
+							<Link href={`mailto:${SUPPORT_EMAIL}`}>{SUPPORT_EMAIL}</Link>
+						</div>
+					</div>
 				</div>
-			</div>
-		)
+			)
+		}
+
+		const message = api.getErrorMessage(error) || 'Unknown error'
+
+		throw new Error(message)
 	}
 }
