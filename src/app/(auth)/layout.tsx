eb8d239427052bdfecc585b2eb165702aaeebdb7
anarkrypto <anarkrypto@gmail.com>
2024-02-17 23:00:49 -0300

    ♻️ Componentize BackgroundIllustration

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,24 +1,8 @@
+import { BackgroundIllustration } from '@/components/BackgroundIllustration'
 import { Container } from '@/components/Container'
 import { Logo } from '@/components/Logo'
 import Link from 'next/link'
 import { ReactNode } from 'react'
-
-function BackgroundIllustration(props: React.ComponentProps<'svg'>) {
-	return (
-		<svg
-			viewBox="0 0 1090 1090"
-			aria-hidden="true"
-			fill="none"
-			preserveAspectRatio="none"
-			{...props}
-		>
-			<circle cx={545} cy={545} r="544.5" />
-			<circle cx={545} cy={545} r="480.5" />
-			<circle cx={545} cy={545} r="416.5" />
-			<circle cx={545} cy={545} r="352.5" />
-		</svg>
-	)
-}
 
 export default function AuthLayout({ children }: { children: ReactNode }) {
 	return (
@@ -28,11 +12,11 @@ export default function AuthLayout({ children }: { children: ReactNode }) {
 					<BackgroundIllustration
 						width="1090"
 						height="1090"
-						className="absolute -top-7 left-1/2 h-[788px] -translate-x-1/2 stroke-nano/20 [mask-image:linear-gradient(to_bottom,white_20%,transparent_75%)] sm:-top-9 sm:h-auto"
+						className="stroke-nano-light absolute -top-7 left-1/2 h-[788px] -translate-x-1/2 [mask-image:linear-gradient(to_bottom,white_20%,transparent_75%)] sm:-top-9 sm:h-auto"
 					/>
 				</div>
 				<div className="z-10 sm:mt-10">
-					<Container className="flex h-screen w-full flex-col justify-center rounded-xl bg-white p-16 shadow-nano/10 sm:h-auto sm:max-w-lg sm:shadow-md">
+					<Container className="shadow-nano/10 flex h-screen w-full flex-col justify-center rounded-xl bg-white p-16 sm:h-auto sm:max-w-lg sm:shadow-md">
 						<div className="mb-6">
 							<Link href="/" aria-label="Home">
 								<Logo theme="light" className="mx-auto h-12 w-auto" />
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,4 +1,3 @@
-import { useId } from 'react'
 import Image from 'next/image'
 
 import { AppScreen } from '@/components/AppScreen'
@@ -17,80 +16,12 @@ import {
 } from 'lucide-react'
 import { Nunito } from 'next/font/google'
 import clsx from 'clsx'
+import { AnimatedBackgroundIllustration } from './BackgroundIllustration'
 
 const nunito = Nunito({
 	weight: ['500'],
 	subsets: ['latin'],
 })
-
-function BackgroundIllustration(props: React.ComponentProps<'div'>) {
-	let id = useId()
-
-	return (
-		<div {...props}>
-			<svg
-				viewBox="0 0 1026 1026"
-				fill="none"
-				aria-hidden="true"
-				className="animate-spin-slow absolute inset-0 h-full w-full"
-			>
-				<path
-					d="M1025 513c0 282.77-229.23 512-512 512S1 795.77 1 513 230.23 1 513 1s512 229.23 512 512Z"
-					stroke="#D4D4D4"
-					strokeOpacity="0.7"
-				/>
-				<path
-					d="M513 1025C230.23 1025 1 795.77 1 513"
-					stroke={`url(#${id}-gradient-1)`}
-					strokeLinecap="round"
-				/>
-				<defs>
-					<linearGradient
-						id={`${id}-gradient-1`}
-						x1="1"
-						y1="513"
-						x2="1"
-						y2="1025"
-						gradientUnits="userSpaceOnUse"
-					>
-						<stop stopColor="#209CE9" />
-						<stop offset="1" stopColor="#209CE9" stopOpacity="0" />
-					</linearGradient>
-				</defs>
-			</svg>
-			<svg
-				viewBox="0 0 1026 1026"
-				fill="none"
-				aria-hidden="true"
-				className="animate-spin-reverse-slower absolute inset-0 h-full w-full"
-			>
-				<path
-					d="M913 513c0 220.914-179.086 400-400 400S113 733.914 113 513s179.086-400 400-400 400 179.086 400 400Z"
-					stroke="#D4D4D4"
-					strokeOpacity="0.7"
-				/>
-				<path
-					d="M913 513c0 220.914-179.086 400-400 400"
-					stroke={`url(#${id}-gradient-2)`}
-					strokeLinecap="round"
-				/>
-				<defs>
-					<linearGradient
-						id={`${id}-gradient-2`}
-						x1="913"
-						y1="513"
-						x2="913"
-						y2="913"
-						gradientUnits="userSpaceOnUse"
-					>
-						<stop stopColor="#209CE9" />
-						<stop offset="1" stopColor="#209CE9" stopOpacity="0" />
-					</linearGradient>
-				</defs>
-			</svg>
-		</div>
-	)
-}
 
 function AppDemo() {
 	return (
@@ -215,7 +146,7 @@ export function Hero() {
 						</div>
 					</div>
 					<div className="relative mt-10 sm:mt-8 lg:col-span-5 lg:row-span-2 lg:mt-0 xl:col-span-6">
-						<BackgroundIllustration className="absolute left-1/2 top-4 h-[1026px] w-[1026px] -translate-x-1/3 stroke-gray-300/70 sm:top-16 sm:-translate-x-1/2 sm:[mask-image:linear-gradient(to_bottom,white_20%,transparent_75%)] lg:-top-16 lg:ml-12 xl:-top-14 xl:ml-0" />
+						<AnimatedBackgroundIllustration className="absolute left-1/2 top-4 h-[1026px] w-[1026px] -translate-x-1/3 stroke-gray-300/70 sm:top-16 sm:-translate-x-1/2 sm:[mask-image:linear-gradient(to_bottom,white_20%,transparent_75%)] lg:-top-16 lg:ml-12 xl:-top-14 xl:ml-0" />
 						<div className="-mx-4 h-[448px] px-9 sm:mx-0 sm:[mask-image:linear-gradient(to_bottom,white_60%,transparent)] lg:absolute lg:-inset-x-10 lg:-bottom-20 lg:-top-10 lg:h-auto lg:px-0 lg:pt-10 xl:-bottom-32">
 							<PhoneFrame className="mx-auto max-w-[366px]" priority>
 								<AppDemo />
