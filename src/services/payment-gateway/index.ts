00d5440c4a5e1124602fc47b48aac6328a5bd6a6
anarkrypto <anarkrypto@gmail.com>
2024-03-09 21:24:37 -0300

    ✨ Add payment-gateway GET /invoice api

--- src/services/payment-gateway/index.ts
+++ src/services/payment-gateway/index.ts
@@ -1,5 +1,6 @@
-import Fetcher from '@/lib/fetcher'
-import { InvoiceCreate } from '@/types/invoice'
+import Fetcher, { FetcherOptions } from '@/lib/fetcher'
+import { InvoiceCreate, InvoiceStatus } from '@/types/invoice'
+import { Service } from '@/types/services'
 
 const fetcher = new Fetcher(process.env.PAYMENT_WORKER_URL!)
 
@@ -21,17 +22,51 @@ export interface CreateInvoiceResponse {
 	expires_at: string
 }
 
+export type FullInvoiceWithService = {
+	id: string
+	title: string
+	index: number
+	status: InvoiceStatus
+	currency: string
+	description: string | null
+	expires_at: string
+	metadata: Record<string, any> | null
+	pay_address: string
+	price: number
+	received_amount: number
+	recipient_address: string
+	redirect_url: string | null
+	refunded_amount: number
+	service_id: string | null
+	user_id: string | null
+	created_at: string
+	service: Service | null
+}
+
 const paymentGateway = {
 	client: fetcher,
 	getErrorMessage: (error: any): string => {
 		return error.message
 	},
 	invoices: {
+		get: async (
+			id: string,
+			options: FetcherOptions,
+		): Promise<FullInvoiceWithService> => {
+			return fetcher.get(`/invoices/${id}`, null, {
+				...options,
+				headers: {
+					'Content-Type': 'application/json',
+					Authorization: `Bearer ${process.env.PAYMENT_WORKER_AUTH_TOKEN!}`,
+					...options.headers,
+				},
+			})
+		},
 		create: async (
 			data: CreateInvoiceRequest,
 		): Promise<CreateInvoiceResponse> => {
 			// Ask Payment Worker to watch for payments
-			return fetcher.post('/', data, {
+			return fetcher.post('/invoices', data, {
 				headers: {
 					'Content-Type': 'application/json',
 					Authorization: `Bearer ${process.env.PAYMENT_WORKER_AUTH_TOKEN!}`,
