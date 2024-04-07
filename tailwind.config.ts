ad2d0fe54849581d68a864992c18a2f319842e26
anarkrypto <anarkrypto@gmail.com>
2024-04-07 12:34:40 -0300

    ➕ Add tailwind-scrollbar

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -87,7 +87,7 @@ const config = {
 			},
 		},
 	},
-	plugins: [require('tailwindcss-animate')],
+	plugins: [require('tailwindcss-animate'), require('tailwind-scrollbar')],
 } satisfies Config
 
 export default config
