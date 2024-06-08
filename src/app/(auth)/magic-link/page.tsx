c50255945901031acf293effafb4c8f5c1d4ce07
anarkrypto <anarkrypto@gmail.com>
2024-06-08 01:32:27 -0300

    refactor magic-link form

--- src/app/(auth)/magic-link/page.tsx
+++ src/app/(auth)/magic-link/page.tsx
@@ -1,16 +1,22 @@
 'use client'
 
-import { useRouter } from 'next/navigation'
 import Link from 'next/link'
 import Input from '@/components/Input'
 import { ajvResolver } from '@hookform/resolvers/ajv'
 import { JSONSchemaType } from 'ajv'
 import { fullFormats } from 'ajv-formats/dist/formats'
-import { Controller, useForm } from 'react-hook-form'
+import { useForm } from 'react-hook-form'
 import { useToast } from '@/hooks/useToast'
 import { useTransition } from 'react'
 import { sendMagicLink } from './actions'
 import { Button } from '@/components/Button'
+import {
+	Form,
+	FormControl,
+	FormField,
+	FormItem,
+	FormMessage,
+} from '@/components/ui/form'
 
 interface MagicEmail {
 	email: string
@@ -27,15 +33,9 @@ const schema: JSONSchemaType<MagicEmail> = {
 export default function MagicLinkPage() {
 	const [isPending, startTransition] = useTransition()
 
-	const router = useRouter()
-
 	const { showError } = useToast()
 
-	const {
-		control,
-		handleSubmit,
-		formState: { errors, isSubmitting },
-	} = useForm<MagicEmail>({
+	const form = useForm<MagicEmail>({
 		defaultValues: {
 			email: '',
 		},
@@ -61,24 +61,38 @@ export default function MagicLinkPage() {
 
 	return (
 		<div className="flex w-full flex-col space-y-6 divide-y divide-slate-200 px-2 sm:px-4">
-			<form className="w-full py-6" onSubmit={handleSubmit(onSubmit)}>
-				<Controller
-					name="email"
-					control={control}
-					render={({ field }) => (
-						<Input
-							label="E-mail"
-							{...field}
-							errorMessage={errors.email?.message}
-							className="w-full !bg-transparent"
-						/>
-					)}
-				/>
+			<Form {...form}>
+				<form
+					className="w-full space-y-4 py-6"
+					onSubmit={form.handleSubmit(onSubmit)}
+				>
+					<FormField
+						name="email"
+						control={form.control}
+						render={({ field, fieldState }) => (
+							<FormItem>
+								<FormControl>
+									<Input
+										label="E-mail"
+										{...field}
+										invalid={fieldState.invalid}
+									/>
+								</FormControl>
+								<FormMessage />
+							</FormItem>
+						)}
+					/>
 
-				<Button type="submit" className="w-full" disabled={isSubmitting}>
-					Send Magic Link
-				</Button>
-			</form>
+					<Button
+						type="submit"
+						className="w-full"
+						loading={form.formState.isSubmitting || isPending}
+						disabled={!form.formState.isDirty}
+					>
+						Send Magic Link
+					</Button>
+				</form>
+			</Form>
 			<div className="flex flex-col items-center py-6">
 				<h2 className="text-base font-semibold text-slate-600">
 					Back to{' '}
