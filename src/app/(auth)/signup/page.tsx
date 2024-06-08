ed2a6e692f8c05448cdb5ccc31719c8e16d76efe
anarkrypto <anarkrypto@gmail.com>
2024-06-08 01:51:51 -0300

    refactor signup form

--- src/app/(auth)/signup/page.tsx
+++ src/app/(auth)/signup/page.tsx
@@ -9,11 +9,18 @@ import Input from '@/components/Input'
 import { ajvResolver } from '@hookform/resolvers/ajv'
 import { JSONSchemaType } from 'ajv'
 import { fullFormats } from 'ajv-formats/dist/formats'
-import { Controller, useForm } from 'react-hook-form'
+import { useForm } from 'react-hook-form'
 import { useToast } from '@/hooks/useToast'
 import { signWithGithub } from '../login/actions'
 import { useTransition } from 'react'
 import { signUpWithPassword } from './actions'
+import {
+	Form,
+	FormControl,
+	FormField,
+	FormItem,
+	FormMessage,
+} from '@/components/ui/form'
 
 interface SignUpEmailPassword {
 	email: string
@@ -36,11 +43,7 @@ export default function SignUpPage() {
 
 	const { showError } = useToast()
 
-	const {
-		control,
-		handleSubmit,
-		formState: { errors, isSubmitting },
-	} = useForm<SignUpEmailPassword>({
+	const form = useForm<SignUpEmailPassword>({
 		defaultValues: {
 			email: '',
 			password: '',
@@ -66,66 +69,77 @@ export default function SignUpPage() {
 	}
 
 	return (
-		<form
-			className="flex w-full flex-col space-y-6 divide-y divide-slate-200 px-2 sm:px-4"
-			onSubmit={handleSubmit(onSubmit)}
-		>
-			<Button
-				color="slate"
-				type="button"
-				onClick={() => signWithGithub({ next })}
-				variant="outline"
+		<Form {...form}>
+			<form
+				className="flex w-full flex-col space-y-6 divide-y divide-slate-200 px-2 sm:px-4"
+				onSubmit={form.handleSubmit(onSubmit)}
 			>
-				<div className="flex items-center space-x-2">
-					<Image src={GithubSVG} width={20} height={20} alt="github icon" />
-					<span>Sign up with Github</span>
-				</div>
-			</Button>
-			<div className="w-full py-6">
-				<Controller
-					name="email"
-					control={control}
-					render={({ field }) => (
-						<Input
-							label="E-mail"
-							{...field}
-							errorMessage={errors.email?.message}
-							className="w-full !bg-transparent"
-						/>
-					)}
-				/>
-
-				<Controller
-					name="password"
-					control={control}
-					render={({ field }) => (
-						<Input
-							label="Password"
-							type="password"
-							{...field}
-							onChange={e => field.onChange(e.target.value.slice(0, 40))}
-							errorMessage={errors.password?.message}
-							className="w-full !bg-transparent"
-						/>
-					)}
-				/>
-
 				<Button
-					type="submit"
-					className="w-full"
-					loading={isSubmitting || isPending}
+					color="slate"
+					type="button"
+					onClick={() => signWithGithub({ next })}
+					variant="outline"
 				>
-					Sign Up
+					<div className="flex items-center space-x-2">
+						<Image src={GithubSVG} width={20} height={20} alt="github icon" />
+						<span>Sign up with Github</span>
+					</div>
 				</Button>
-			</div>
-			<div className="flex flex-col items-center py-6">
-				<h2 className="text-base font-semibold text-slate-600">
-					Already have an account ?{' '}
-					<Link href="/login" className="text-nano underline">
-						Sign In
-					</Link>
-				</h2>
-			</div>
-		</form>
+				<div className="w-full space-y-4 py-6">
+					<FormField
+						name="email"
+						control={form.control}
+						render={({ field, fieldState }) => (
+							<FormItem>
+								<FormControl>
+									<Input
+										label="E-mail"
+										{...field}
+										invalid={fieldState.invalid}
+									/>
+								</FormControl>
+								<FormMessage />
+							</FormItem>
+						)}
+					/>
+
+					<FormField
+						name="password"
+						control={form.control}
+						render={({ field, fieldState }) => (
+							<FormItem>
+								<FormControl>
+									<Input
+										label="Password"
+										type="password"
+										{...field}
+										onChange={e => field.onChange(e.target.value.slice(0, 40))}
+										invalid={fieldState.invalid}
+									/>
+								</FormControl>
+								<FormMessage />
+							</FormItem>
+						)}
+					/>
+
+					<Button
+						type="submit"
+						className="w-full"
+						loading={form.formState.isSubmitting || isPending}
+						disabled={!form.formState.isDirty}
+					>
+						Sign Up
+					</Button>
+				</div>
+				<div className="flex flex-col items-center py-6">
+					<h2 className="text-base font-semibold text-slate-600">
+						Already have an account ?{' '}
+						<Link href="/login" className="text-nano underline">
+							Sign In
+						</Link>
+					</h2>
+				</div>
+			</form>
+		</Form>
 	)
 }
