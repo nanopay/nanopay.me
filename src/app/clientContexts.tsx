3f47b1ea4a15632396a5e1d0855d29284538f708
anarkrypto <anarkrypto@gmail.com>
2024-06-08 00:53:47 -0300

    remove @mui/material

--- src/app/clientContexts.tsx
+++ src/app/clientContexts.tsx
@@ -1,8 +1,6 @@
 'use client'
 
 import { ReactNode } from 'react'
-import { theme } from '@/styles/mui-theme'
-import { ThemeProvider } from '@mui/material'
 import { ToastContainer } from 'react-toastify'
 import { QueryClient, QueryClientProvider } from 'react-query'
 
@@ -11,10 +9,8 @@ const queryClient = new QueryClient()
 export default function ClientContexts({ children }: { children: ReactNode }) {
 	return (
 		<QueryClientProvider client={queryClient}>
-			<ThemeProvider theme={theme}>
-				{children}
-				<ToastContainer />
-			</ThemeProvider>
+			{children}
+			<ToastContainer />
 		</QueryClientProvider>
 	)
 }
