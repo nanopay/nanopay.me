93a7b80539724d93947c464decfe82ede9201ecb
anarkrypto <anarkrypto@gmail.com>
2024-06-11 04:07:51 -0300

    Refactor complete-profile form

--- src/app/(setup)/complete-profile/complete-profile-form.tsx
+++ src/app/(setup)/complete-profile/complete-profile-form.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import { useToast } from '@/hooks/useToast'
-import { useForm, Controller } from 'react-hook-form'
+import { useForm } from 'react-hook-form'
 import Image from 'next/image'
 import Input from '@/components/Input'
 import { useState, useTransition } from 'react'
@@ -15,6 +15,13 @@ import { Button } from '@/components/Button'
 import { Checkbox } from '@/components/ui/checkbox'
 import { CheckedState } from '@radix-ui/react-checkbox'
 import Link from 'next/link'
+import {
+	Form,
+	FormControl,
+	FormField,
+	FormItem,
+	FormMessage,
+} from '@/components/ui/form'
 
 const schema: JSONSchemaType<UserEditables> = {
 	type: 'object',
@@ -42,12 +49,7 @@ export default function CompleteProfileForm({
 
 	const [acceptTerms, setAcceptTerms] = useState(false)
 
-	const {
-		control,
-		handleSubmit,
-		getValues,
-		formState: { errors, isSubmitting },
-	} = useForm<UserEditables>({
+	const form = useForm<UserEditables>({
 		defaultValues: {
 			name: initialData.name,
 			avatar_url: initialData.avatar_url || DEFAULT_AVATAR_URL,
@@ -77,75 +79,65 @@ export default function CompleteProfileForm({
 	}
 
 	return (
-		<form
-			onSubmit={handleSubmit(onSubmit)}
-			className="flex w-full max-w-sm flex-col items-center space-y-6"
-		>
-			<Image
-				src={getValues('avatar_url') || ''}
-				alt="Logo"
-				width={128}
-				height={128}
-				className="rounded-full border-2 border-slate-200"
-				priority
-			/>
-
-			<Controller
-				name="name"
-				control={control}
-				render={({ field }) => (
-					<Input
-						label="Name *"
-						{...field}
-						onChange={e => field.onChange(e.target.value.slice(0, 40))}
-						errorMessage={errors.name?.message}
-						className="w-full"
-						autoCapitalize="words"
-						style={{
-							textTransform: 'capitalize',
-						}}
-					/>
-				)}
-			/>
-
-			<Input
-				label="E-mail"
-				value={initialData.email}
-				errorMessage={!initialData ? 'Missing email' : undefined}
-				className="w-full"
-				InputLabelProps={{
-					shrink: true,
-				}}
-				disabled
-			/>
-			<div className="flex select-none items-center gap-2">
-				<Checkbox
-					id="terms"
-					checked={acceptTerms}
-					onCheckedChange={handleAcceptTerms}
-				/>
-				<label
-					htmlFor="terms"
-					className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
-				>
-					I agree to the{' '}
-					<Link href="/terms" target="_blank" className="text-nano">
-						terms of service
-					</Link>
-				</label>
-			</div>
-			<Button
-				type="submit"
-				loading={isSubmitting || isPending}
-				disabled={
-					!getValues('name') ||
-					!initialData.email ||
-					!getValues('avatar_url') ||
-					!acceptTerms
-				}
+		<Form {...form}>
+			<form
+				onSubmit={form.handleSubmit(onSubmit)}
+				className="flex w-full max-w-sm flex-col items-center space-y-4"
 			>
-				Register Me
-			</Button>
-		</form>
+				<Image
+					src={form.watch('avatar_url') || ''}
+					alt="Logo"
+					width={128}
+					height={128}
+					className="rounded-full border-2 border-slate-200"
+					priority
+				/>
+
+				<FormField
+					name="name"
+					control={form.control}
+					render={({ field, fieldState }) => (
+						<FormItem>
+							<FormControl>
+								<Input
+									label="Name *"
+									{...field}
+									onChange={e => field.onChange(e.target.value.slice(0, 40))}
+									invalid={fieldState.invalid}
+									className="capitalize"
+								/>
+							</FormControl>
+							<FormMessage />
+						</FormItem>
+					)}
+				/>
+
+				<Input name="email" label="E-mail" value={initialData.email} disabled />
+
+				<div className="flex select-none items-center gap-2">
+					<Checkbox
+						id="terms"
+						checked={acceptTerms}
+						onCheckedChange={handleAcceptTerms}
+					/>
+					<label
+						htmlFor="terms"
+						className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+					>
+						I agree to the{' '}
+						<Link href="/terms" target="_blank" className="text-nano">
+							terms of service
+						</Link>
+					</label>
+				</div>
+				<Button
+					type="submit"
+					loading={form.formState.isSubmitting || isPending}
+					disabled={!form.watch('name') || !initialData.email || !acceptTerms}
+				>
+					Register Me
+				</Button>
+			</form>
+		</Form>
 	)
 }
