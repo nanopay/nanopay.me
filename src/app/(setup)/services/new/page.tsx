5ea4484af878304cc9618675d80469d3da24b5ab
anarkrypto <anarkrypto@gmail.com>
2024-06-11 04:23:56 -0300

    refactor new service form

--- src/app/(setup)/services/new/page.tsx
+++ src/app/(setup)/services/new/page.tsx
@@ -1,9 +1,8 @@
 'use client'
 
 import { useState, useTransition } from 'react'
-import { Controller, FieldErrors, useForm } from 'react-hook-form'
+import { useForm } from 'react-hook-form'
 import { fullFormats } from 'ajv-formats/dist/formats'
-import { Container } from '@/components/Container'
 import Input from '@/components/Input'
 import { ajvResolver } from '@hookform/resolvers/ajv'
 import api from '@/services/api'
@@ -16,6 +15,14 @@ import { createAvatarUploadPresignedUrl, createService } from './actions'
 import { uploadObject } from '@/services/s3'
 import { InfoIcon } from 'lucide-react'
 import { Button } from '@/components/Button'
+import {
+	Form,
+	FormControl,
+	FormField,
+	FormItem,
+	FormMessage,
+} from '@/components/ui/form'
+import { TextArea } from '@/components/TextArea'
 
 const schema: JSONSchemaType<ServiceCreate> = {
 	type: 'object',
@@ -51,13 +58,7 @@ export default function NewService() {
 
 	const [isPending, startTransition] = useTransition()
 
-	const {
-		control,
-		handleSubmit,
-		watch,
-		setValue,
-		formState: { errors },
-	} = useForm<ServiceCreate>({
+	const form = useForm<ServiceCreate>({
 		resolver: ajvResolver(schema, {
 			formats: fullFormats,
 		}),
@@ -76,83 +77,86 @@ export default function NewService() {
 		})
 	}
 
-	const onErrorSubmiting = (e: FieldErrors<ServiceCreate>) => {
-		showError(
-			'Error creating service',
-			e.avatar_url
-				? `avatar_url ${e.avatar_url.message}`
-				: 'Check the fields entered',
-		)
-	}
-
 	return (
-		<Container className="flex h-screen w-full max-w-xl flex-col items-center space-y-6 border border-slate-200 bg-white px-16 pb-16 sm:h-auto sm:rounded-lg">
-			<div className="mb-8 flex w-full items-center justify-center border-b border-slate-200 py-3">
-				<h3 className="text-slate-700">Create a new service</h3>
-			</div>
+		<Form {...form}>
+			<form
+				className="flex h-screen w-full max-w-xl flex-col items-center space-y-6 border border-slate-200 bg-white px-16 pb-16 sm:h-auto sm:rounded-lg"
+				onSubmit={form.handleSubmit(onSubmit)}
+			>
+				<div className="mb-8 flex w-full items-center justify-center border-b border-slate-200 py-3">
+					<h3 className="text-slate-700">Create a new service</h3>
+				</div>
 
-			<ServiceAvatar
-				url={watch('avatar_url') || undefined}
-				onChange={url => {
-					console.log('new url', url)
-					setValue('avatar_url', url)
-				}}
-				onUploading={setIsUploading}
-			/>
+				<ServiceAvatar
+					url={form.watch('avatar_url') || undefined}
+					onChange={url => {
+						console.log('new url', url)
+						form.setValue('avatar_url', url)
+					}}
+					onUploading={setIsUploading}
+				/>
 
-			<div className="flex w-full flex-col space-y-6 px-4 py-6 sm:px-8">
-				<div>
-					<div className="mb-2 flex items-center text-xs text-slate-600">
-						<InfoIcon className="mr-1 w-4" />
-						<div>
-							Use a name like: <span className="font-semibold">my-service</span>
-							{' or '}
-							<span className="font-semibold">myservice2.com</span>
+				<div className="flex w-full flex-col space-y-6 px-4 py-6 sm:px-8">
+					<div>
+						<div className="mb-2 flex items-center text-xs text-slate-600">
+							<InfoIcon className="mr-1 w-4" />
+							<div>
+								Use a name like:{' '}
+								<span className="font-semibold">my-service</span>
+								{' or '}
+								<span className="font-semibold">myservice2.com</span>
+							</div>
 						</div>
+						<FormField
+							name="name"
+							control={form.control}
+							render={({ field, fieldState }) => (
+								<FormItem>
+									<FormControl>
+										<Input
+											label="Name"
+											{...field}
+											onChange={e =>
+												field.onChange(sanitizeSlug(e.target.value))
+											}
+											invalid={fieldState.invalid}
+											className="capitalize"
+										/>
+									</FormControl>
+									<FormMessage />
+								</FormItem>
+							)}
+						/>
 					</div>
-					<Controller
-						name="name"
-						control={control}
-						render={({ field }) => (
-							<Input
-								label="Name"
-								{...field}
-								onChange={e => field.onChange(sanitizeSlug(e.target.value))}
-								errorMessage={errors.name?.message}
-								className="w-full"
-								autoCapitalize="words"
-								style={{
-									textTransform: 'capitalize',
-								}}
-							/>
+
+					<FormField
+						name="description"
+						control={form.control}
+						render={({ field, fieldState }) => (
+							<FormItem>
+								<FormControl>
+									<TextArea
+										label="Description"
+										{...field}
+										value={field.value || ''}
+										onChange={e => field.onChange(e.target.value.slice(0, 512))}
+										invalid={fieldState.invalid}
+									/>
+								</FormControl>
+								<FormMessage />
+							</FormItem>
 						)}
 					/>
 				</div>
-
-				<Controller
-					name="description"
-					control={control}
-					render={({ field }) => (
-						<Input
-							label="Description"
-							{...field}
-							onChange={e => field.onChange(e.target.value.slice(0, 512))}
-							errorMessage={errors.description?.message}
-							className="w-full"
-							multiline={true}
-						/>
-					)}
-				/>
-			</div>
-			<div />
-			<Button
-				onClick={handleSubmit(fields => onSubmit(fields), onErrorSubmiting)}
-				loading={isPending}
-				disabled={isUploading || !watch('name')}
-			>
-				Create Service
-			</Button>
-		</Container>
+				<div />
+				<Button
+					loading={form.formState.isSubmitting || isPending}
+					disabled={isUploading || !form.watch('name')}
+				>
+					Create Service
+				</Button>
+			</form>
+		</Form>
 	)
 }
 
