bac13f32235bd1a78b09fd5e7008214446b2d314
anarkrypto <anarkrypto@gmail.com>
2024-06-14 03:22:45 -0300

    remove react-query

--- src/app/clientContexts.tsx
+++ src/app/clientContexts.tsx
@@ -2,15 +2,12 @@
 
 import { ReactNode } from 'react'
 import { ToastContainer } from 'react-toastify'
-import { QueryClient, QueryClientProvider } from 'react-query'
-
-const queryClient = new QueryClient()
 
 export default function ClientContexts({ children }: { children: ReactNode }) {
 	return (
-		<QueryClientProvider client={queryClient}>
+		<>
 			{children}
 			<ToastContainer />
-		</QueryClientProvider>
+		</>
 	)
 }
