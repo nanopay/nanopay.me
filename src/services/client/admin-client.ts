64bca2baac49b1070dd1eb42b48d4727d562201c
anarkrypto <anarkrypto@gmail.com>
2024-06-25 19:29:46 -0300

    add admin-client

---
+++ src/services/client/admin-client.ts
@@ -0,0 +1,20 @@
+import { InvoicesService } from './invoices/invoices-service'
+import { ServicesService } from './services/services-service'
+import { createClient } from '@supabase/supabase-js'
+import { Database } from '@/types/supabase'
+
+// BE CAREFUL! Never expose this client to other parts of the application.
+const supabase = createClient<Database>(
+	process.env.SUPABASE_URL!,
+	process.env.SUPABASE_SECRET_KEY!,
+)
+
+export class AdminClient {
+	readonly services: ServicesService
+	readonly invoices: InvoicesService
+
+	constructor() {
+		this.services = new ServicesService(supabase)
+		this.invoices = new InvoicesService(supabase)
+	}
+}
