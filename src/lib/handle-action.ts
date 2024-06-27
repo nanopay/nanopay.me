c31cdc699a375cfaf81eecc934849009d0b52f93
anarkrypto <anarkrypto@gmail.com>
2024-06-27 19:06:59 -0300

    add action handler

---
+++ src/lib/handle-action.ts
@@ -0,0 +1,29 @@
+type ActionFunction<T> = (...args: any[]) => Promise<T>
+
+export interface ActionErrorResponse {
+	_error: string
+}
+
+export interface ActionSuccessResponse<T> {
+	_data: T
+}
+
+/**
+ * Handles execution and error handling for an asynchronous action function.
+ * @param action The asynchronous action function to be handled.
+ * @returns A function that, when called, executes the action function and handles any errors.
+ */
+export const handleAction =
+	<T>(action: ActionFunction<T>) =>
+	async (
+		...args: any[]
+	): Promise<ActionSuccessResponse<T> | ActionErrorResponse> => {
+		try {
+			const result = await action(...args)
+			return { _data: result }
+		} catch (error) {
+			return {
+				_error: error instanceof Error ? error.message : 'An error occurred',
+			}
+		}
+	}
