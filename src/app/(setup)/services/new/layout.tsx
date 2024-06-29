995239b8ed02f6a5b71b3f49e6d2799106807c44
anarkrypto <anarkrypto@gmail.com>
2024-06-29 18:19:45 -0300

    refactor: in NewServiceLayout redirect if profile is missing

--- src/app/(setup)/services/new/layout.tsx
+++ src/app/(setup)/services/new/layout.tsx
@@ -1,14 +1,22 @@
+import { getCachedUser } from '@/lib/cache/user'
 import { Metadata } from 'next'
+import { redirect } from 'next/navigation'
 
 export const metadata: Metadata = {
 	title: 'New Service',
 	description: 'Create a new service in NanoPay',
 }
 
-export default function NewServiceLayout({
+export default async function NewServiceLayout({
 	children,
 }: {
 	children: React.ReactNode
 }) {
+	const user = await getCachedUser()
+
+	if (!user) {
+		redirect('/complete-profile')
+	}
+
 	return children
 }
