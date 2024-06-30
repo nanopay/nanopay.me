54445345c1e302e4295594aa028f8577ec2ad8b6
anarkrypto <anarkrypto@gmail.com>
2024-06-30 12:31:22 -0300

    refactor: Add ApiKeysService to AdminClient

--- src/services/client/admin-client.ts
+++ src/services/client/admin-client.ts
@@ -2,19 +2,21 @@ import { InvoicesService } from './invoices/invoices-service'
 import { ServicesService } from './services/services-service'
 import { createClient } from '@supabase/supabase-js'
 import { Database } from '@/types/database'
-
-// BE CAREFUL! Never expose this client to other parts of the application.
-const supabase = createClient<Database>(
-	process.env.SUPABASE_URL!,
-	process.env.SUPABASE_SECRET_KEY!,
-)
-
+import { ApiKeysService } from './api-keys'
 export class AdminClient {
 	readonly services: ServicesService
 	readonly invoices: InvoicesService
+	readonly apiKeys: ApiKeysService
 
 	constructor() {
+		// BE CAREFUL! Never expose this client to other parts of the application.
+		const supabase = createClient<Database>(
+			process.env.SUPABASE_URL!,
+			process.env.SUPABASE_SECRET_KEY!,
+		)
+
 		this.services = new ServicesService(supabase)
 		this.invoices = new InvoicesService(supabase)
+		this.apiKeys = new ApiKeysService(supabase)
 	}
 }
