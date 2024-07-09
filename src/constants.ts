ace0541d284c1bdcbe3d2d73746f162dc4d07505
anarkrypto <anarkrypto@gmail.com>
2024-07-09 20:42:30 -0300

    feat: Add support for additional image types in ALLOWED_IMAGE_TYPES constant

--- src/constants.ts
+++ src/constants.ts
@@ -13,4 +13,9 @@ export const STATIC_ASSETS_HOST = process.env.NEXT_PUBLIC_STATIC_ASSETS_HOST!
 
 export const DEFAULT_AVATAR_URL = 'https://static.nanopay.me/images/user.png'
 export const MAX_IMAGE_SIZE = 1024 * 1024 * 8 // 8 MB
-export const ALLOWED_IMAGE_TYPES = ['image/png']
+export const ALLOWED_IMAGE_TYPES = [
+	'image/png',
+	'image/jpeg',
+	'image/jpg',
+	'image/webp',
+]
