ddee364f33a6524135db8abbc0de7870fa3359b5
anarkrypto <anarkrypto@gmail.com>
2024-07-09 23:31:59 -0300

    feat: Add useUploader hook for file uploading functionality

---
+++ src/hooks/useUploader.ts
@@ -0,0 +1,107 @@
+import { useState } from 'react'
+
+export const uploadFileWithXHR = async (
+	file: File,
+	url: string,
+	progressCallback?: (progress: number) => void,
+): Promise<{ url: string }> => {
+	return new Promise((resolve, reject) => {
+		const xhr = new XMLHttpRequest()
+		xhr.open('POST', url, true)
+
+		xhr.upload.onprogress = function (event) {
+			if (progressCallback && event.lengthComputable) {
+				const percentComplete = (event.loaded / event.total) * 100
+				progressCallback(percentComplete)
+			}
+		}
+
+		xhr.onload = function () {
+			if (xhr.status === 201) {
+				try {
+					const response = JSON.parse(xhr.responseText)
+					const url = response.url
+					new URL(url)
+					resolve({ url })
+				} catch (error) {
+					reject('Error uploading file. Invalid response')
+				}
+			} else {
+				reject('Error uploading file. Status: ' + xhr.status)
+			}
+		}
+
+		xhr.onerror = function () {
+			reject('Error uploading file. Check your network connection.')
+		}
+
+		const formData = new FormData()
+		formData.append('file', file)
+
+		xhr.send(formData)
+	})
+}
+
+export type UploaderHookOptions = {
+	onSuccess?: (url: string) => void
+	onError?: (message: string) => void
+	onUploading?: (bool: boolean) => void
+}
+
+const uploaderHook = (apiUrl: string, options: UploaderHookOptions = {}) => {
+	const [isUploading, setIsUploading] = useState(false)
+	const [isError, setIsError] = useState(false)
+	const [isSuccess, setIsSuccess] = useState(false)
+	const [progress, setProgress] = useState(0)
+
+	const reset = () => {
+		setIsUploading(false)
+		setIsError(false)
+		setIsSuccess(false)
+		setProgress(0)
+	}
+
+	return {
+		upload: async (image: File): Promise<void> => {
+			reset()
+			setIsUploading(true)
+			try {
+				await uploadFileWithXHR(image, apiUrl, setProgress)
+				setIsSuccess(true)
+				if (options.onSuccess) {
+					options.onSuccess(apiUrl)
+				}
+				if (options.onUploading) {
+					options.onUploading(true)
+				}
+			} catch (error) {
+				setIsError(true)
+				if (options.onError) {
+					options.onError(
+						error instanceof Error ? error.message : 'Unknown error',
+					)
+				}
+			} finally {
+				setIsUploading(false)
+				if (options.onUploading) {
+					options.onUploading(false)
+				}
+			}
+		},
+		isUploading,
+		isSuccess,
+		isError,
+		progress,
+	}
+}
+
+export const useServiceAvatarUploader = (
+	serviceNameOrId: string,
+	options: UploaderHookOptions,
+) => {
+	return uploaderHook(`/${serviceNameOrId}/avatar`, options)
+}
+
+export const useUserAvatarUploader = (options: UploaderHookOptions) => {
+	return uploaderHook(`/profile/avatar`, options)
+}
