50798971450dc8682a91b7bd1c2125b930d20e8b
anarkrypto <anarkrypto@gmail.com>
2024-07-09 23:27:49 -0300

    feat: Add route for uploading and updating user avatar image

---
+++ src/app/(dashboard)/profile/avatar/route.ts
@@ -0,0 +1,75 @@
+import { MAX_IMAGE_SIZE, STATIC_ASSETS_HOST } from '@/constants'
+import { Client } from '@/services/client'
+import { putObject } from '@/services/s3'
+import { ServerRuntime } from 'next'
+import { revalidateTag } from 'next/cache'
+import { cookies } from 'next/headers'
+import { NextRequest } from 'next/server'
+
+export const runtime: ServerRuntime = 'edge'
+
+export async function POST(req: NextRequest) {
+	try {
+		const formData = await req.formData()
+		const file = formData.get('file') as File
+
+		if (!file) {
+			return Response.json(
+				{
+					message: 'No file received',
+				},
+				{
+					status: 400,
+				},
+			)
+		}
+
+		if (file.type !== 'image/png') {
+			return Response.json(
+				{
+					message: 'Invalid image type',
+				},
+				{
+					status: 400,
+				},
+			)
+		}
+
+		if (file.size > MAX_IMAGE_SIZE) {
+			return Response.json(
+				{
+					message: 'Image too large',
+				},
+				{
+					status: 400,
+				},
+			)
+		}
+
+		const client = new Client(cookies())
+
+		const userId = await client.user.getUserId()
+
+		const key = `users/${userId}/avatar.png`
+
+		await putObject(key, file, file.type)
+
+		const url = new URL(STATIC_ASSETS_HOST)
+		url.pathname = key
+		url.searchParams.append('v', Date.now().toString())
+
+		await client.user.updateProfile({ avatar_url: url.toString() })
+
+		revalidateTag(`user-${userId}`)
+
+		return Response.json(
+			{ url: url.toString() },
+			{
+				status: 201,
+			},
+		)
+	} catch (error) {
+		console.error(error)
+		return Response.json({ message: 'Error uploading' }, { status: 500 })
+	}
+}
