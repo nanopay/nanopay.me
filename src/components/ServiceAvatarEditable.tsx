981c62059620ebedc974a4e39dd895daf55cc5ca
anarkrypto <anarkrypto@gmail.com>
2024-07-09 23:33:05 -0300

    feat: Add ServiceAvatarEditable component for editing service avatars

---
+++ src/components/ServiceAvatarEditable.tsx
@@ -0,0 +1,46 @@
+'use client'
+
+import { useToast } from '@/hooks/useToast'
+import ImageInput from './ImageInput'
+import { ALLOWED_IMAGE_TYPES } from '@/constants'
+import { useServiceAvatarUploader } from '@/hooks/useUploader'
+
+export interface ServiceAvatarEditableProps {
+	id: string
+	src?: string | null
+	size?: number
+	alt?: string
+	onChange?: (url: string) => void
+	onUploading?: (bool: boolean) => void
+}
+
+export function ServiceAvatarEditable({
+	id,
+	src,
+	size = 98,
+	alt = 'Service Avatar',
+	onChange,
+	onUploading,
+}: ServiceAvatarEditableProps) {
+	const { showError } = useToast()
+
+	const { upload, isUploading, progress, isError } = useServiceAvatarUploader(
+		id,
+		{ onSuccess: onChange, onUploading, onError: showError },
+	)
+
+	return (
+		<ImageInput
+			src={src || `https://avatar.vercel.sh/${id}`}
+			crop={true}
+			onChange={upload}
+			isLoading={isUploading}
+			isError={isError}
+			progress={progress}
+			width={size}
+			height={size}
+			alt={alt}
+			acceptTypes={ALLOWED_IMAGE_TYPES}
+		/>
+	)
+}
