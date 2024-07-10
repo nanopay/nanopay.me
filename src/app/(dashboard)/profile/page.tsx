900330b39e07030952317deea0a3931a03063149
anarkrypto <anarkrypto@gmail.com>
2024-07-09 23:33:37 -0300

    refactor: Remove unused code and update profile page

--- src/app/(dashboard)/profile/page.tsx
+++ src/app/(dashboard)/profile/page.tsx
@@ -3,19 +3,9 @@
 import Input from '@/components/Input'
 import { useUser } from '@/contexts/UserProvider'
 import { useToast } from '@/hooks/useToast'
-import { ajvResolver } from '@hookform/resolvers/ajv'
-import { JSONSchemaType } from 'ajv'
-import { fullFormats } from 'ajv-formats/dist/formats'
-import { useState, useTransition } from 'react'
+import { useTransition } from 'react'
 import { useForm } from 'react-hook-form'
-import {
-	UpdateUserProps,
-	createAvatarUploadPresignedUrl,
-	updateAvatar,
-	updateUser,
-} from './actions'
-import ImageInput from '@/components/ImageInput'
-import { uploadObject } from '@/services/s3'
+import { UpdateUserProps, updateUser } from './actions'
 import { Button } from '@/components/Button'
 import {
 	Form,
@@ -24,16 +14,14 @@ import {
 	FormItem,
 	FormMessage,
 } from '@/components/ui/form'
-import { UserUpdate } from '@/services/client'
-import { DEFAULT_AVATAR_URL } from '@/constants'
+import { UserUpdate, userNameSchema } from '@/services/client'
+import { UserAvatarEditable } from '@/components/UserAvatarEditable'
+import { z } from 'zod'
+import { zodResolver } from '@hookform/resolvers/zod'
 
-const schema: JSONSchemaType<UpdateUserProps> = {
-	type: 'object',
-	properties: {
-		name: { type: 'string', minLength: 2, maxLength: 40 },
-	},
-	required: ['name'],
-}
+const schema = z.object({
+	name: userNameSchema,
+})
 
 export default function Profile() {
 	const user = useUser()
@@ -46,9 +34,7 @@ export default function Profile() {
 		defaultValues: {
 			name: user.name,
 		},
-		resolver: ajvResolver(schema, {
-			formats: fullFormats,
-		}),
+		resolver: zodResolver(schema),
 	})
 
 	const onSubmit = async ({ name }: Partial<UserUpdate>) => {
@@ -71,7 +57,7 @@ export default function Profile() {
 				onSubmit={form.handleSubmit(onSubmit)}
 				className="flex w-full max-w-sm flex-col items-center space-y-6"
 			>
-				<UserAvatar url={user.avatar_url || DEFAULT_AVATAR_URL} />
+				<UserAvatarEditable src={user.avatar_url} alt={user.name} />
 
 				<div className="flex w-full flex-col space-y-4">
 					<FormField
@@ -104,45 +90,5 @@ export default function Profile() {
 				</Button>
 			</form>
 		</Form>
-	)
-}
-
-function UserAvatar({ url }: { url: string }) {
-	const [progress, setProgress] = useState(0)
-	const [isError, setIsError] = useState(false)
-	const [isPending, startTransition] = useTransition()
-	const { showError } = useToast()
-
-	const handleUploadAvatar = async (file: File) => {
-		startTransition(async () => {
-			try {
-				const url = await createAvatarUploadPresignedUrl({
-					type: file.type,
-					size: file.size,
-				})
-
-				await uploadObject(file, url, setProgress)
-				await updateAvatar()
-			} catch (error) {
-				setIsError(true)
-				showError(
-					'Error uploading avatar',
-					error instanceof Error
-						? error.message
-						: 'Check the data or try again later.',
-				)
-			}
-		})
-	}
-
-	return (
-		<ImageInput
-			source={url}
-			crop={true}
-			onChange={handleUploadAvatar}
-			isLoading={isPending}
-			isError={isError}
-			progress={progress}
-		/>
 	)
 }
