5df265db36848ff993c9739bdb19adfce1a12d3b
anarkrypto <anarkrypto@gmail.com>
2024-07-09 23:42:52 -0300

    refactor: Remove unused code related to avatar handling in profile actions

--- src/app/(dashboard)/profile/actions.ts
+++ src/app/(dashboard)/profile/actions.ts
@@ -2,13 +2,7 @@
 
 import { cookies } from 'next/headers'
 import { getUserId } from '@/utils/supabase/server'
-import {
-	ALLOWED_IMAGE_TYPES,
-	MAX_IMAGE_SIZE,
-	STATIC_ASSETS_URL,
-} from '@/constants'
 import { revalidateTag } from 'next/cache'
-import { createPresignedUrl, moveObject } from '@/services/s3'
 import { Client } from '@/services/client'
 
 export interface UpdateUserProps {
@@ -22,59 +16,6 @@ export const updateUser = async ({ name }: Partial<UpdateUserProps>) => {
 
 	await client.user.updateProfile({
 		name,
-	})
-
-	revalidateTag(`user-${userId}`)
-}
-
-export const createAvatarUploadPresignedUrl = async ({
-	type,
-	size,
-}: {
-	type: string
-	size: number
-}) => {
-	const userId = await getUserId(cookies())
-
-	if (!ALLOWED_IMAGE_TYPES.includes(type)) {
-		throw new Error('Invalid file type')
-	}
-
-	if (size < 32) {
-		throw new Error('Image too small')
-	}
-
-	if (size > MAX_IMAGE_SIZE) {
-		throw new Error('Image too large')
-	}
-
-	const key = `tmp/users/${userId}/avatar.png`
-
-	const url = await createPresignedUrl({
-		key,
-		type,
-		size,
-	})
-
-	return url
-}
-
-export const updateAvatar = async () => {
-	const userId = await getUserId(cookies())
-
-	const oldKey = `tmp/users/${userId}/avatar.png`
-	const newKey = `users/${userId}/avatar.png`
-
-	await moveObject(oldKey, newKey)
-
-	const avatarUrl = new URL(STATIC_ASSETS_URL)
-	avatarUrl.pathname = newKey
-	avatarUrl.searchParams.set('v', Date.now().toString())
-
-	const client = new Client(cookies())
-
-	await client.user.updateProfile({
-		avatar_url: avatarUrl.toString(),
 	})
 
 	revalidateTag(`user-${userId}`)
