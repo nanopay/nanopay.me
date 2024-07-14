7c060bda4a3d1fc643116e1830253b207481ddbb
anarkrypto <anarkrypto@gmail.com>
2024-07-14 03:57:49 -0300

    refactor: Use safeAction for deleting an API key

--- src/app/(dashboard)/[serviceName]/keys/actions.ts
+++ src/app/(dashboard)/[serviceName]/keys/actions.ts
@@ -1,15 +1,15 @@
 'use server'
 
+import { safeAction } from '@/lib/safe-action'
 import { Client } from '@/services/client'
 import { revalidateTag } from 'next/cache'
 import { cookies } from 'next/headers'
+import { z } from 'zod'
 
-export async function deleteApiKey(apiKeyId: string) {
-	try {
+export const deleteApiKey = safeAction
+	.schema(z.string())
+	.action(async ({ parsedInput: apiKeyId }) => {
 		const client = new Client(cookies())
 		await client.apiKeys.delete(apiKeyId)
 		revalidateTag('')
-	} catch (error) {
-		console.error('Failed to delete API key:', error)
-	}
-}
+	})
--- src/app/(dashboard)/[serviceName]/keys/delete.tsx
+++ src/app/(dashboard)/[serviceName]/keys/delete.tsx
@@ -2,15 +2,28 @@
 
 import { Button } from '@/components/Button'
 import { deleteApiKey } from './actions'
+import { useAction } from 'next-safe-action/hooks'
+import { useToast } from '@/hooks/useToast'
+import { getSafeActionError } from '@/lib/safe-action'
 
 export default function DeleteApiKeyButton({ apiKeyId }: { apiKeyId: string }) {
+	const { showError } = useToast()
+
+	const { executeAsync, isExecuting } = useAction(deleteApiKey, {
+		onError: ({ error }) => {
+			const { message } = getSafeActionError(error)
+			showError('Could not delete API key', message)
+		},
+	})
+
 	return (
 		<Button
 			onClick={async () => {
-				await deleteApiKey(apiKeyId)
+				await executeAsync(apiKeyId)
 			}}
 			variant="destructive"
 			size="sm"
+			loading={isExecuting}
 		>
 			Delete
 		</Button>
