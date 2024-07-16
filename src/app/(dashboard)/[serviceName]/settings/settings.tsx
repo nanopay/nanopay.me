19de737cd947a7ace0b416a7ea69e484c9502729
anarkrypto <anarkrypto@gmail.com>
2024-07-16 20:23:31 -0300

    feat: Add DeleteServiceAlertModal

--- src/app/(dashboard)/[serviceName]/settings/settings.tsx
+++ src/app/(dashboard)/[serviceName]/settings/settings.tsx
@@ -1,6 +1,14 @@
 'use client'
 
-import { GlobeIcon, TrashIcon } from 'lucide-react'
+import {
+	AlertTriangleIcon,
+	GlobeIcon,
+	KeyRoundIcon,
+	ReceiptIcon,
+	TrashIcon,
+	WebhookIcon,
+	XIcon,
+} from 'lucide-react'
 import { Button } from '@/components/Button'
 import { deleteService } from './actions'
 import { Service } from '@/services/client'
@@ -8,7 +16,20 @@ import { ServiceAvatarEditable } from '@/components/ServiceAvatarEditable'
 import { useAction } from 'next-safe-action/hooks'
 import { useToast } from '@/hooks/useToast'
 import { getSafeActionError } from '@/lib/safe-action'
-
+import {
+	Dialog,
+	DialogClose,
+	DialogContent,
+	DialogDescription,
+	DialogFooter,
+	DialogHeader,
+	DialogTitle,
+} from '@/components/ui/dialog'
+import { useState } from 'react'
+import { DialogProps } from '@radix-ui/react-dialog'
+import Input from '@/components/Input'
+import { ServiceAvatar } from '@/components/ServiceAvatar'
+import { Alert, AlertDescription, AlertTitle } from '@/components/ui/alert'
 export interface SettingsProps {
 	service: Service
 }
@@ -16,16 +37,10 @@ export interface SettingsProps {
 export function Settings({ service }: SettingsProps) {
 	const { showError } = useToast()
 
-	const { execute: executeDeleteService, isExecuting: isDeletingService } =
-		useAction(deleteService, {
-			onError: ({ error }) => {
-				const { message } = getSafeActionError(error)
-				showError(message)
-			},
-		})
+	const [openDeleteServiceAlert, setOpenDeleteServiceAlert] = useState(false)
 
-	const handleDeleteService = () => {
-		executeDeleteService(service.name)
+	const handleOpenDeleteServiceAlert = () => {
+		setOpenDeleteServiceAlert(true)
 	}
 
 	return (
@@ -87,8 +102,7 @@ export function Settings({ service }: SettingsProps) {
 						<Button
 							variant="destructive"
 							color="nano"
-							onClick={handleDeleteService}
-							loading={isDeletingService}
+							onClick={handleOpenDeleteServiceAlert}
 						>
 							<TrashIcon className="-ml-1 mr-2 h-4 w-4" aria-hidden="true" />
 							Delete
@@ -96,6 +110,147 @@ export function Settings({ service }: SettingsProps) {
 					</div>
 				</div>
 			</div>
+			<DeleteServiceAlertModal
+				service={service}
+				open={openDeleteServiceAlert}
+				onOpenChange={setOpenDeleteServiceAlert}
+			/>
 		</header>
+	)
+}
+
+export interface DeleteServiceAlertModalProps extends DialogProps {
+	service: Service
+}
+
+export function DeleteServiceAlertModal({
+	service,
+	...props
+}: DeleteServiceAlertModalProps) {
+	const { showError } = useToast()
+
+	const [wantToDelete, setWantToDelete] = useState(false)
+	const [understandEffects, setUnderstandEffects] = useState(false)
+	const [typeConfirm, setTypeConfirm] = useState('')
+
+	const { execute, isExecuting } = useAction(deleteService, {
+		onError: ({ error }) => {
+			const { message } = getSafeActionError(error)
+			showError(message)
+		},
+	})
+
+	const handleWantToDelete = () => {
+		setWantToDelete(true)
+	}
+
+	const handleUnderstandEffects = () => {
+		setUnderstandEffects(true)
+	}
+
+	const handleDeleteService = () => {
+		execute(service.name)
+	}
+
+	return (
+		<Dialog modal {...props}>
+			<DialogContent className="divide-x-slate-200 gap-0 divide-y p-0">
+				<DialogHeader className="flex flex-row items-center justify-between px-4 py-2">
+					<DialogTitle>Delete your service?</DialogTitle>
+					<DialogClose>
+						<Button
+							size="icon"
+							variant="outline"
+							color="slate"
+							className="border-0 focus:bg-slate-100"
+							autoFocus
+						>
+							<XIcon className="h-5 w-5" />
+						</Button>
+					</DialogClose>
+				</DialogHeader>
+				<div className="flex flex-col items-center p-4">
+					<ServiceAvatar
+						id={service.id}
+						size={64}
+						src={service.avatar_url}
+						alt={service.display_name}
+					/>
+					<p className="mt-2 text-lg font-bold">{service.display_name}</p>
+					<ul className="flex items-center space-x-2 divide-x divide-slate-200 p-4 text-sm text-slate-600">
+						<li className="flex flex-col items-center px-4">
+							<ReceiptIcon className="h-4 w-4" />
+							<p>{service.invoices_count} invoices</p>
+						</li>
+						<li className="flex flex-col items-center px-4">
+							<KeyRoundIcon className="h-4 w-4" />
+							<p>{service.api_keys_count} API Keys</p>
+						</li>
+						<li className="flex flex-col items-center px-4">
+							<WebhookIcon className="h-4 w-4" />
+							<p>{service.webhooks_count} Webhooks</p>
+						</li>
+					</ul>
+				</div>
+				{wantToDelete && !understandEffects && (
+					<div className="w-full p-4">
+						<Alert className="border-yellow-700 bg-yellow-50 text-slate-800">
+							<AlertTriangleIcon className="h-5 w-5 !text-yellow-500" />
+							<AlertTitle>Are you absolutely sure?</AlertTitle>
+							<AlertDescription>
+								This action cannot be undone. This will permanently delete your
+								service <b>{service.name}</b> and remove your service data from
+								our servers, including all invoices, payments, webhooks, api
+								keys, and other data.
+							</AlertDescription>
+						</Alert>
+					</div>
+				)}
+				{understandEffects && (
+					<div className="w-full p-4">
+						<p className="py-2 font-medium">
+							To confirm, type &quot;
+							<b>{service.name}</b>&quot; in the box below
+						</p>
+						<Input
+							invalid={typeConfirm !== service.name}
+							onChange={e => {
+								setTypeConfirm(e.target.value)
+							}}
+						/>
+					</div>
+				)}
+				<DialogFooter className="p-4">
+					{(understandEffects && (
+						<Button
+							variant="destructive"
+							loading={isExecuting}
+							onClick={handleDeleteService}
+							className="w-full"
+							disabled={typeConfirm !== service.name}
+						>
+							Delete this service
+						</Button>
+					)) ||
+						(wantToDelete && (
+							<Button
+								variant="destructive"
+								onClick={handleUnderstandEffects}
+								className="w-full"
+							>
+								I have read and understand these effects
+							</Button>
+						)) || (
+							<Button
+								variant="destructive"
+								onClick={handleWantToDelete}
+								className="w-full"
+							>
+								I want delete this service
+							</Button>
+						)}
+				</DialogFooter>
+			</DialogContent>
+		</Dialog>
 	)
 }
