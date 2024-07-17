b2ea0891ed15152f00ef43f44811b892cb597b11
anarkrypto <anarkrypto@gmail.com>
2024-07-16 21:10:06 -0300

    improve: Update settings page UI components and structure

--- src/app/(dashboard)/[serviceName]/settings/settings.tsx
+++ src/app/(dashboard)/[serviceName]/settings/settings.tsx
@@ -30,6 +30,14 @@ import { DialogProps } from '@radix-ui/react-dialog'
 import Input from '@/components/Input'
 import { ServiceAvatar } from '@/components/ServiceAvatar'
 import { Alert, AlertDescription, AlertTitle } from '@/components/ui/alert'
+import {
+	Card,
+	CardDescription,
+	CardFooter,
+	CardHeader,
+	CardTitle,
+} from '@/components/ui/card'
+import Link from 'next/link'
 export interface SettingsProps {
 	service: Service
 }
@@ -44,23 +52,15 @@ export function Settings({ service }: SettingsProps) {
 	}
 
 	return (
-		<header className="rounded-lg bg-white shadow">
-			<div className="px-4 sm:px-6 lg:mx-auto lg:max-w-6xl lg:px-8">
-				<div className="py-6 md:flex md:items-center md:justify-between">
-					<div className="min-w-0 flex-1">
-						{/* Service Profile */}
-						<div className="flex items-center">
-							<div className="hidden sm:flex">
-								<ServiceAvatarEditable
-									id={service.id}
-									size={64}
-									src={service.avatar_url}
-									alt={service.display_name}
-								/>
-							</div>
-							<div>
+		<>
+			<div className="flex flex-col space-y-8">
+				<header className="rounded-lg bg-white shadow">
+					<div className="px-4 sm:px-6 lg:mx-auto lg:max-w-6xl lg:px-8">
+						<div className="py-6 md:flex md:items-center md:justify-between">
+							<div className="min-w-0 flex-1">
+								{/* Service Profile */}
 								<div className="flex items-center">
-									<div className="sm:hidden">
+									<div className="hidden sm:flex">
 										<ServiceAvatarEditable
 											id={service.id}
 											size={64}
@@ -68,54 +68,84 @@ export function Settings({ service }: SettingsProps) {
 											alt={service.display_name}
 										/>
 									</div>
-									<h1 className="ml-3 text-2xl font-bold leading-7 text-slate-900 sm:truncate sm:leading-9">
-										{service.name}
-									</h1>
-								</div>
-								<dl className="mt-6 flex flex-col sm:ml-3 sm:mt-1 sm:flex-row sm:flex-wrap">
-									<dt className="sr-only">Website</dt>
-									{service.website && (
-										<dd className="sm:mr-6">
-											<a
-												href={service.website}
-												className="hover:text-nano flex items-center truncate text-sm font-medium text-slate-500"
-											>
-												<GlobeIcon
-													className="mr-1 h-5 w-5 flex-shrink-0"
-													aria-hidden="true"
+									<div>
+										<div className="flex items-center">
+											<div className="sm:hidden">
+												<ServiceAvatarEditable
+													id={service.id}
+													size={64}
+													src={service.avatar_url}
+													alt={service.display_name}
 												/>
-												{service.website}
-											</a>
-										</dd>
-									)}
-									{service.description && (
-										<dd className="flex items-center text-sm font-medium text-slate-500 sm:mr-6">
-											{service.description.slice(0, 60)}
-											{service.description.length > 60 && '...'}
-										</dd>
-									)}
-								</dl>
+											</div>
+											<h1 className="ml-3 text-2xl font-bold leading-7 text-slate-900 sm:truncate sm:leading-9">
+												{service.name}
+											</h1>
+										</div>
+										<dl className="mt-6 flex flex-col sm:ml-3 sm:mt-1 sm:flex-row sm:flex-wrap">
+											<dt className="sr-only">Website</dt>
+											{service.website && (
+												<dd className="sm:mr-6">
+													<a
+														href={service.website}
+														className="hover:text-nano flex items-center truncate text-sm font-medium text-slate-500"
+													>
+														<GlobeIcon
+															className="mr-1 h-5 w-5 flex-shrink-0"
+															aria-hidden="true"
+														/>
+														{service.website}
+													</a>
+												</dd>
+											)}
+											{service.description && (
+												<dd className="flex items-center text-sm font-medium text-slate-500 sm:mr-6">
+													{service.description.slice(0, 60)}
+													{service.description.length > 60 && '...'}
+												</dd>
+											)}
+										</dl>
+									</div>
+								</div>
 							</div>
 						</div>
 					</div>
-					<div className="mt-6 flex space-x-3 md:ml-4 md:mt-0">
-						<Button
-							variant="destructive"
-							color="nano"
-							onClick={handleOpenDeleteServiceAlert}
-						>
-							<TrashIcon className="-ml-1 mr-2 h-4 w-4" aria-hidden="true" />
-							Delete
-						</Button>
-					</div>
-				</div>
+				</header>
+				<Card className="w-full divide-y divide-red-200 overflow-hidden border-red-200">
+					<CardHeader>
+						<CardTitle>Delete Service</CardTitle>
+						<CardDescription>
+							Permanently remove your service and all of its contents from the
+							NanoPay.me platform. This action is not reversible — please
+							continue with caution.
+						</CardDescription>
+					</CardHeader>
+					<CardFooter className="bg-red-50 pt-6">
+						<div className="flex w-full items-center justify-between">
+							<p className="font-medium text-red-600">
+								To delete your account, visit{' '}
+								<Link href="/profile" className="text-nano hover:underline">
+									Account Settings.
+								</Link>
+							</p>
+							<Button
+								variant="destructive"
+								onClick={handleOpenDeleteServiceAlert}
+								className="font-semibold"
+							>
+								<TrashIcon className="-ml-1 mr-2 h-4 w-4" aria-hidden="true" />
+								Delete Service
+							</Button>
+						</div>
+					</CardFooter>
+				</Card>
 			</div>
 			<DeleteServiceAlertModal
 				service={service}
 				open={openDeleteServiceAlert}
 				onOpenChange={setOpenDeleteServiceAlert}
 			/>
-		</header>
+		</>
 	)
 }
 
