679e6960698273231c141d8be2a4529f426f2617
anarkrypto <anarkrypto@gmail.com>
2024-07-19 07:39:04 -0300

    refactor: Update checkUUID function to return boolean type

--- src/core/utils.ts
+++ src/core/utils.ts
@@ -11,7 +11,7 @@ export const slugify = (str: string): string => {
 		.toLowerCase() // Lowercase the slug.
 }
 
-export const checkUUID = (str: string) => {
+export const checkUUID = (str: string): boolean => {
 	const regex = new RegExp(
 		'^[0-9a-f]{8}-[0-9a-f]{4}-4[0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$',
 		'gi',
