2d751f5acd5e45e5c09e935b311b1dc84e17c5be
anarkrypto <anarkrypto@gmail.com>
2024-07-23 17:14:43 -0300

    feat: Add LoadingIndicator

---
+++ src/components/LoadingIndicator.tsx
@@ -0,0 +1,31 @@
+'use client'
+
+import { useRouter } from 'next/navigation'
+import { useEffect, useOptimistic } from 'react'
+import NProgress from 'nprogress'
+
+export function LoadingIndicator() {
+	const router = useRouter()
+	const [loading, setLoading] = useOptimistic(false)
+
+	useEffect(() => {
+		NProgress.configure({ showSpinner: false })
+		if (router.push.name === 'patched') return
+		const push = router.push
+		router.push = function patched(...args) {
+			setLoading(true)
+			push.apply(history, args)
+		}
+	}, [])
+
+	useEffect(() => {
+		if (loading) {
+			NProgress.start()
+		}
+		return () => {
+			NProgress.done()
+		}
+	}, [loading])
+
+	return null
+}
