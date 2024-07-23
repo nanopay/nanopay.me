00b9b693712e785492cea7c64dadde78eda4d519
anarkrypto <anarkrypto@gmail.com>
2024-07-23 13:37:42 -0300

    style: Update Footer component styling and theme

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -10,19 +10,22 @@ import { CONTACT_EMAIL } from '@/core/constants'
 
 export function Footer() {
 	return (
-		<footer className="mt-8 border-t border-slate-200">
+		<footer className="mt-8 border-t border-slate-200 bg-slate-900">
 			<Container>
 				<div className="flex flex-col items-start justify-between gap-y-12 pb-6 pt-16 lg:flex-row lg:items-center lg:py-8">
 					<div>
-						<div className="flex items-center text-slate-900">
-							<Logomark className="h-auto w-12 flex-none fill-blue-500" />
+						<div className="flex items-center text-slate-100">
+							<Logomark
+								theme="dark"
+								className="h-auto w-12 flex-none fill-blue-500"
+							/>
 							<div className="ml-4">
 								<p className="text-base font-semibold">NanoPay.me</p>
 								<p className="text-sm">Pay and receive payments with Nano.</p>
 							</div>
 						</div>
 						<nav className="mt-11 flex gap-8">
-							<NavLinks />
+							<NavLinks theme="dark" />
 						</nav>
 					</div>
 					<div className="hover:bg-nano/10 group relative -mx-4 flex items-center self-stretch p-4 transition-colors sm:self-auto sm:rounded-2xl lg:mx-0 lg:self-auto lg:p-6">
@@ -31,22 +34,22 @@ export function Footer() {
 							<Image src={xnoHeart} alt="XNO" className="h-14 w-14" />
 						</div>
 						<div className="ml-8 lg:w-64">
-							<p className="text-base font-semibold text-slate-900">
+							<p className="text-base font-semibold text-slate-100">
 								<Link href="#">
 									<span className="absolute inset-0 sm:rounded-2xl" />
 									Donate with Nano
 								</Link>
 							</p>
-							<p className="mt-1 text-sm text-slate-700">
+							<p className="mt-1 text-sm text-slate-300">
 								Donate with NanoPay.me
 							</p>
 						</div>
 					</div>
 				</div>
-				<div className="flex flex-col items-center border-t border-slate-200 py-8 md:flex-row-reverse md:justify-between md:pt-6">
+				<div className="flex flex-col items-center border-t border-slate-700 py-8 md:flex-row-reverse md:justify-between md:pt-6">
 					<Link
 						href={`mailto:${CONTACT_EMAIL}`}
-						className="text-sm text-slate-500"
+						className="text-sm text-slate-400"
 					>
 						{CONTACT_EMAIL}
 					</Link>
