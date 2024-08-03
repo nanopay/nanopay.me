d4d8ae96455d09a04e16ecd3c9614778c0fd9fb1
anarkrypto <anarkrypto@gmail.com>
2024-08-03 14:39:27 -0300

    refactor: Update Invoices component to handle empty results and improve pagination

--- src/components/Invoices.tsx
+++ src/components/Invoices.tsx
@@ -26,7 +26,7 @@ export default async function Invoices({
 	serviceIdOrSlug,
 	showPagination = true,
 }: InvoicesProps) {
-	const from = offset + 1
+	const from = invoices.length > 0 ? offset + 1 : 0
 	const to = invoices.length + offset
 	const previousPage = from > 1 && Math.floor(from / 10)
 	const nextPage = to < count && Math.floor(to / 10) + 1
@@ -34,72 +34,90 @@ export default async function Invoices({
 	return (
 		<>
 			{/* Activity list (smallest breakpoint only) */}
-			<div className="border border-slate-200 sm:hidden">
-				<ul
-					role="list"
-					className="mt-2 divide-y divide-slate-200 overflow-hidden shadow sm:hidden"
-				>
-					{invoices?.map(invoice => (
-						<li key={invoice.id}>
-							<a
-								href={`/invoices/${invoice.id}`}
-								className="block bg-white px-4 py-4 hover:bg-slate-100"
-							>
-								<span className="flex items-center space-x-4">
-									<span className="flex flex-1 space-x-2 truncate">
-										<BanknoteIcon
+			<div className="divide-y divide-slate-200 overflow-hidden rounded-lg border border-slate-200 sm:hidden">
+				{count > 0 && (
+					<ul
+						role="list"
+						className="mt-2 divide-y divide-slate-200 overflow-hidden shadow sm:hidden"
+					>
+						{invoices?.map(invoice => (
+							<li key={invoice.id}>
+								<a
+									href={`/invoices/${invoice.id}`}
+									className="block bg-white px-4 py-4 hover:bg-slate-100"
+								>
+									<span className="flex items-center space-x-4">
+										<span className="flex flex-1 space-x-2 truncate">
+											<BanknoteIcon
+												className="h-5 w-5 flex-shrink-0 text-slate-400"
+												aria-hidden="true"
+											/>
+											<span className="flex flex-col truncate text-sm text-slate-500">
+												<span className="truncate">{invoice.title}</span>
+												<span>
+													<span className="font-medium text-slate-900">
+														{invoice.price}
+													</span>{' '}
+													{invoice.currency}
+												</span>
+												<time dateTime={invoice.created_at}>
+													{formatDateTime(invoice.created_at)}
+												</time>
+											</span>
+										</span>
+										<ChevronRightIcon
 											className="h-5 w-5 flex-shrink-0 text-slate-400"
 											aria-hidden="true"
 										/>
-										<span className="flex flex-col truncate text-sm text-slate-500">
-											<span className="truncate">{invoice.title}</span>
-											<span>
-												<span className="font-medium text-slate-900">
-													{invoice.price}
-												</span>{' '}
-												{invoice.currency}
-											</span>
-											<time dateTime={invoice.created_at}>
-												{formatDateTime(invoice.created_at)}
-											</time>
-										</span>
 									</span>
-									<ChevronRightIcon
-										className="h-5 w-5 flex-shrink-0 text-slate-400"
-										aria-hidden="true"
-									/>
-								</span>
-							</a>
-						</li>
-					))}
-				</ul>
+								</a>
+							</li>
+						))}
+					</ul>
+				)}
 
-				<nav
-					className="flex items-center justify-between border-t border-slate-200 bg-white px-4 py-3"
-					aria-label="Pagination"
-				>
-					<div className="flex flex-1 justify-between">
-						<a
-							href="#"
-							className="relative inline-flex items-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-slate-900 ring-1 ring-inset ring-slate-300 hover:bg-slate-100"
-						>
-							Previous
-						</a>
-						<a
-							href="#"
-							className="relative ml-3 inline-flex items-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-slate-900 ring-1 ring-inset ring-slate-300 hover:bg-slate-100"
-						>
-							Next
-						</a>
+				{count > 0 ? (
+					<nav
+						className="flex items-center justify-between border-t border-slate-200 bg-white px-4 py-3"
+						aria-label="Pagination"
+					>
+						<div className="flex flex-1 justify-between">
+							{previousPage ? (
+								<a
+									href="#"
+									className="relative inline-flex items-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-slate-900 ring-1 ring-inset ring-slate-300 hover:bg-slate-100"
+								>
+									Previous
+								</a>
+							) : (
+								<div />
+							)}
+							{nextPage ? (
+								<a
+									href="#"
+									className="relative ml-3 inline-flex items-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-slate-900 ring-1 ring-inset ring-slate-300 hover:bg-slate-100"
+								>
+									Next
+								</a>
+							) : (
+								<div />
+							)}
+						</div>
+					</nav>
+				) : (
+					<div className="flex items-center justify-center bg-white px-4 py-3">
+						<p className="text-sm text-slate-700">
+							<span className="font-medium">No results</span>
+						</p>
 					</div>
-				</nav>
+				)}
 			</div>
 
 			{/* Activity table (small breakpoint and up) */}
 			<div className="hidden sm:block">
 				<div className="mx-auto max-w-7xl">
 					<div className="mt-2 flex flex-col">
-						<div className="min-w-full overflow-hidden overflow-x-auto border border-slate-200 align-middle sm:rounded-lg">
+						<div className="min-w-full overflow-hidden overflow-x-auto rounded-lg border border-slate-200 align-middle">
 							<table className="min-w-full divide-y divide-slate-200">
 								<thead>
 									<tr>
@@ -184,11 +202,17 @@ export default async function Invoices({
 									aria-label="Pagination"
 								>
 									<div className="hidden sm:block">
-										<p className="text-sm text-slate-700">
-											Showing <span className="font-medium">{from}</span> to{' '}
-											<span className="font-medium">{to}</span> of{' '}
-											<span className="font-medium">{count}</span> results
-										</p>
+										{count > 0 ? (
+											<p className="text-sm text-slate-700">
+												Showing <span className="font-medium">{from}</span> to{' '}
+												<span className="font-medium">{to}</span> of{' '}
+												<span className="font-medium">{count}</span> results
+											</p>
+										) : (
+											<p className="text-sm text-slate-700">
+												<span className="font-medium">No results</span>
+											</p>
+										)}
 									</div>
 									<div className="flex flex-1 justify-between gap-x-3 sm:justify-end">
 										{previousPage && (
