764bcb59a4c2b64d08a3aea9c1b58ef71946caf9
anarkrypto <anarkrypto@gmail.com>
2024-08-06 02:28:45 -0300

    improve: style mdx headings with links

--- src/mdx-components.tsx
+++ src/mdx-components.tsx
@@ -1,7 +1,29 @@
+import { LinkIcon } from 'lucide-react'
 import type { MDXComponents } from 'mdx/types'
+import { ReactNode } from 'react'
 
 export function useMDXComponents(components: MDXComponents): MDXComponents {
 	return {
+		h1: ({ ...props }) => (
+			<HeadingLink id={props.id} children={<h1 {...props} />} />
+		),
+		h2: ({ ...props }) => (
+			<HeadingLink id={props.id} children={<h2 {...props} />} />
+		),
+		h3: ({ ...props }) => (
+			<HeadingLink id={props.id} children={<h3 {...props} />} />
+		),
+		h4: ({ ...props }) => (
+			<HeadingLink id={props.id} children={<h4 {...props} />} />
+		),
 		...components,
 	}
+}
+
+function HeadingLink({ id, children }: { id?: string; children: ReactNode }) {
+	return (
+		<a href={`#${id}`} className="text-sm no-underline">
+			{children}
+		</a>
+	)
 }
