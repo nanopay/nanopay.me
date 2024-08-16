c9e513cf8f9d8262c9cbd4a315885843219eb4b2
anarkrypto <anarkrypto@gmail.com>
2024-08-16 02:20:23 -0300

    feat: implement form and action into ServiceNameCard

--- src/app/(dashboard)/[serviceIdOrSlug]/settings/settings-cards/ServiceNameCard.tsx
+++ src/app/(dashboard)/[serviceIdOrSlug]/settings/settings-cards/ServiceNameCard.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import { Button } from '@/components/Button'
 import Input from '@/components/Input'
 import {
@@ -8,9 +10,23 @@ import {
 	CardHeader,
 	CardTitle,
 } from '@/components/ui/card'
-import { Service } from '@/core/client'
+import {
+	Form,
+	FormControl,
+	FormField,
+	FormItem,
+	FormMessage,
+} from '@/components/ui/form'
+import { Service, serviceNameSchema } from '@/core/client'
 import { MAX_SERVICE_NAME_LENGTH } from '@/core/constants'
 import { cn } from '@/lib/cn'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useAction } from 'next-safe-action/hooks'
+import { useForm } from 'react-hook-form'
+import { z } from 'zod'
+import { updateService } from '../actions'
+
+const schema = z.object({ name: serviceNameSchema })
 
 export default function ServiceNameCard({
 	serviceId,
@@ -20,36 +36,75 @@ export default function ServiceNameCard({
 	serviceId: Service['id']
 	value: Service['name']
 } & React.ComponentProps<typeof Card>) {
+	const form = useForm<z.infer<typeof schema>>({
+		defaultValues: {
+			name: value,
+		},
+		resolver: zodResolver(schema),
+	})
+
+	const { executeAsync } = useAction(updateService, {})
+
+	const onSubmit = async (values: z.infer<typeof schema>) => {
+		await executeAsync({
+			serviceIdOrSlug: serviceId,
+			...values,
+		})
+	}
+
+	const isSubmitting = form.formState.isSubmitting
+	const isSubmitDisabled = isSubmitting || !form.formState.isDirty
+
 	return (
-		<Card
-			{...props}
-			className={cn('w-full overflow-hidden border-slate-200', props.className)}
-		>
-			<CardHeader>
-				<CardTitle>Service Name</CardTitle>
-				<CardDescription>
-					This is your service&apos;s visible name within NanoPay.me. For
-					example, the name of your company, organization, project.
-				</CardDescription>
-			</CardHeader>
-			<CardContent>
-				<div className="w-full max-w-sm">
-					<Input value={value} />
-				</div>
-			</CardContent>
-			<CardFooter className="border-t border-slate-200 bg-slate-100 pt-6">
-				<div className="flex w-full items-center justify-between">
-					<p className="text-sm text-slate-600">
-						Please use {MAX_SERVICE_NAME_LENGTH} characters at maximum.
-					</p>
-					<Button
-						// onClick={}
-						className="font-semibold"
-					>
-						Save
-					</Button>
-				</div>
-			</CardFooter>
-		</Card>
+		<Form {...form}>
+			<form onSubmit={form.handleSubmit(onSubmit)}>
+				<Card
+					{...props}
+					className={cn(
+						'w-full overflow-hidden border-slate-200',
+						props.className,
+					)}
+				>
+					<CardHeader>
+						<CardTitle>Service Name</CardTitle>
+						<CardDescription>
+							This is your service&apos;s visible name within NanoPay.me. For
+							example, the name of your company, organization, project.
+						</CardDescription>
+					</CardHeader>
+					<CardContent>
+						<div className="w-full max-w-sm">
+							<FormField
+								name="name"
+								control={form.control}
+								render={({ field, fieldState }) => (
+									<FormItem>
+										<FormControl>
+											<Input {...field} invalid={fieldState.invalid} />
+										</FormControl>
+										<FormMessage />
+									</FormItem>
+								)}
+							/>
+						</div>
+					</CardContent>
+					<CardFooter className="border-t border-slate-200 bg-slate-100 pt-6">
+						<div className="flex w-full items-center justify-between">
+							<p className="text-sm text-slate-600">
+								Please use {MAX_SERVICE_NAME_LENGTH} characters at maximum.
+							</p>
+							<Button
+								type="submit"
+								className="font-semibold"
+								loading={isSubmitting}
+								disabled={isSubmitDisabled}
+							>
+								Save
+							</Button>
+						</div>
+					</CardFooter>
+				</Card>
+			</form>
+		</Form>
 	)
 }
