f56edf4710ec0764cf52702c0134e184b4282ee2
anarkrypto <anarkrypto@gmail.com>
2024-08-16 05:03:53 -0300

    style: increase NavLinks font height to medium

--- src/components/NavLinks.tsx
+++ src/components/NavLinks.tsx
@@ -28,7 +28,7 @@ export function NavLinks({ theme = 'light' }: { theme?: 'light' | 'dark' }) {
 					target={href.startsWith('http') ? '_blank' : undefined}
 					className={cn(
 						cn(
-							'relative -mx-3 -my-2 rounded-lg px-3 py-2 text-sm transition-colors delay-150',
+							'relative -mx-3 -my-2 rounded-lg px-3 py-2 text-sm font-medium transition-colors delay-150',
 							theme === 'light'
 								? 'text-slate-800 hover:text-slate-900'
 								: 'text-slate-300 hover:text-slate-200',
