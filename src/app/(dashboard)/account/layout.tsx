da257c6f9ba9f536c5008b0538b5b350f64d1bf0
anarkrypto <anarkrypto@gmail.com>
2024-09-16 19:13:58 -0300

    style: remove old code and styles from account settings layout

--- src/app/(dashboard)/account/layout.tsx
+++ src/app/(dashboard)/account/layout.tsx
@@ -10,12 +10,5 @@ export default function AccountSettingsLayout({
 }: {
 	children: React.ReactNode
 }) {
-	return (
-		<Container className="flex h-screen w-full max-w-xl flex-col items-center space-y-6 border border-slate-200 bg-white px-16 pb-16 sm:h-auto sm:rounded-lg">
-			<div className="mb-8 flex w-full items-center justify-center border-b border-slate-200 py-3">
-				<h3 className="text-slate-700">Account Settings</h3>
-			</div>
-			{children}
-		</Container>
-	)
+	return children
 }
