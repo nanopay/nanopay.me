56fa2b3c9c1fdee48994a6ea75ea722558fa1927
anarkrypto <anarkrypto@gmail.com>
2024-09-16 19:13:18 -0300

    feat: add deleteUser action

--- src/app/(dashboard)/account/actions.ts
+++ src/app/(dashboard)/account/actions.ts
@@ -3,8 +3,10 @@
 import { cookies } from 'next/headers'
 import { getUserId } from '@/lib/supabase/server'
 import { revalidateTag } from 'next/cache'
-import { Client, userUpdateSchema } from '@/core/client'
+import { AdminClient, Client, userUpdateSchema } from '@/core/client'
 import { safeAction } from '@/lib/safe-action'
+import { z } from 'zod'
+import { redirect } from 'next/navigation'
 
 export const updateUser = safeAction
 	.schema(userUpdateSchema)
@@ -17,3 +19,20 @@ export const updateUser = safeAction
 
 		revalidateTag(`user-${userId}`)
 	})
+
+export const deleteUser = safeAction.schema(z.any()).action(async () => {
+	const userId = await getUserId(cookies())
+
+	const adminClient = new AdminClient()
+	await adminClient.users.delete(userId)
+
+	cookies()
+		.getAll()
+		.forEach(cookie => {
+			cookies().delete(cookie.name)
+		})
+
+	revalidateTag(`user-${userId}`)
+
+	redirect('/')
+})
