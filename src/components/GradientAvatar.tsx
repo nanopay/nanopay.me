b53200a04f90a0818f69a8e776260439b675f998
anarkrypto <anarkrypto@gmail.com>
2024-09-30 18:13:51 -0300

    fix GradientAvatar id to fill - without this change the GradientAvatar would be the same for multiple different uids rendered on the same page

--- src/components/GradientAvatar.tsx
+++ src/components/GradientAvatar.tsx
@@ -27,12 +27,18 @@ export function GradientAvatar({
 		>
 			<g>
 				<defs>
-					<linearGradient id="gradient" x1="0" y1="0" x2="1" y2="1">
+					<linearGradient id={`gradient-${uid}`} x1="0" y1="0" x2="1" y2="1">
 						<stop offset="0%" stopColor={gradient.fromColor} />
 						<stop offset="100%" stopColor={gradient.toColor} />
 					</linearGradient>
 				</defs>
-				<rect fill="url(#gradient)" x="0" y="0" width={size} height={size} />
+				<rect
+					fill={`url(#gradient-${uid})`}
+					x="0"
+					y="0"
+					width={size}
+					height={size}
+				/>
 			</g>
 		</svg>
 	)
