f9cba0da400874e67310ced92a7cc1b6d11fd638
anarkrypto <anarkrypto@gmail.com>
2024-11-22 21:29:55 -0300

    refactor: handle error in auth callback api

--- src/app/(auth)/auth/callback/route.ts
+++ src/app/(auth)/auth/callback/route.ts
@@ -5,26 +5,39 @@ import { NextRequest } from 'next/server'
 export const runtime = 'edge'
 
 export async function GET(request: NextRequest) {
-	const requestUrl = new URL(request.url)
-	const code = requestUrl.searchParams.get('code')
-	const next = requestUrl.searchParams.get('next')
+	try {
+		const requestUrl = new URL(request.url)
+		const code = requestUrl.searchParams.get('code')
+		const next = requestUrl.searchParams.get('next')
 
-	if (!code) {
+		if (!code) {
+			return Response.json(
+				{
+					error: 'code is missing',
+				},
+				{
+					status: 400,
+				},
+			)
+		}
+
+		const client = new Client(cookies())
+		await client.auth.exchangeCodeForSession(code)
+
+		const redirectTo = new URL(requestUrl.origin)
+		redirectTo.pathname = next || '/'
+
+		return Response.redirect(next || '/')
+	} catch (error) {
+		const message = error instanceof Error ? error.message : 'An error occurred'
 		return Response.json(
 			{
-				error: 'code is missing',
+				status: 'error',
+				message,
 			},
 			{
-				status: 400,
+				status: 500,
 			},
 		)
 	}
-
-	const client = new Client(cookies())
-	await client.auth.exchangeCodeForSession(code)
-
-	const redirectTo = new URL(requestUrl.origin)
-	redirectTo.pathname = next || '/'
-
-	return Response.redirect(redirectTo.toString())
 }
